6ef4538f74ff3d295b5720ee3ca0d2f31cdda3a4
JYKIM317 <augu202@naver.com>
2024-11-03 20:09:09 +0900

    refactor: RTMP Handshake 과정을 모듈로 분리
    RTMP Handshake 함수를 /core/rtmp/handshake.ts로 분리하였고,
    그 외 절대 경로 표시를 위한 tsconfig-paths 모듈 설치와 tsconfig.json에 path 별칭을 등록하였다.
    
    Co-authored-by: zzawang <hobinchoi0116@gmail.com>

---
+++ mediaServer/core/rtmp/handshake.ts
@@ -0,0 +1,53 @@
+import net from 'net';
+
+const isC0 = (data: Buffer) => data.length === 1;
+const isC1 = (C0: Buffer, C1: Buffer) => C0.length === 1 && C1.length === 0;
+const isC0C1 = (data: Buffer) => data.length == 1537;
+const isC2 = (C1: Buffer, C2: Buffer) => C1.length === 1536 && C2.length === 0;
+const isBufferSize1536 = (data: Buffer) => data.length === 1536;
+const isValidC2RandomEcho = (S1_RANDOM_ECHO: Buffer, C2_RANDOM_ECHO: Buffer) => S1_RANDOM_ECHO.equals(C2_RANDOM_ECHO);
+
+type HandshakeData = {
+  [key: string]: Buffer;
+};
+
+function handshake(socket: net.Socket, data: Buffer, state: HandshakeData) {
+  // RTMP Handshake Packet 할당
+  if (isC0(data)) {
+    state.C0 = data;
+  } else if (isC0C1(data)) {
+    state.C0 = data.subarray(0, 1);
+    state.C1 = data.subarray(1);
+  } else if (isBufferSize1536(data)) {
+    if (isC1(state.C0, state.C1)) {
+      state.C1 = data;
+    } else if (isC2(state.C1, state.C2)) {
+      state.C2 = data;
+    }
+  }
+
+  // C1 Packet 분리 할당
+  if (isBufferSize1536(state.C1) || isC0C1(data)) {
+    state.C1_TIMESTAMP = state.C1.subarray(0, 4);
+    state.C1_RANDOM_BYTES = state.C1.subarray(8);
+  }
+
+  // RTMP Handshake Packet 전송
+  if (isC0(data) || isC0C1(data)) {
+    const S0 = Buffer.alloc(1);
+    S0.writeUIntLE(3, 0, 1);
+    socket.write(S0);
+
+    const S1_0 = Buffer.alloc(4);
+    const S1 = Buffer.concat([state.S1_TIMESTAMP, S1_0, state.S1_RANDOM_BYTES]);
+    socket.write(S1);
+  } else if (isBufferSize1536(state.C2)) {
+    const C2_RANDOM_ECHO = state.C2.subarray(8);
+    if (!isValidC2RandomEcho(state.S1_RANDOM_BYTES, C2_RANDOM_ECHO)) return;
+
+    const S2 = Buffer.concat([state.C1_TIMESTAMP, state.S1_TIMESTAMP, state.C1_RANDOM_BYTES]);
+    socket.write(S2);
+  }
+}
+
+export { HandshakeData, handshake };
