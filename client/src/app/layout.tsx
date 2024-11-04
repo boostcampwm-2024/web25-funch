df66d3d287f5593040f34f54dbfb49deeb928dd6
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-04 20:31:19 +0900

    setting: 폰트 className설정
    - Noto_Sans_KR className을 body에 추가함

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${notoSansKR.className} antialiased`}>{children}</body>
     </html>
   );
 }
