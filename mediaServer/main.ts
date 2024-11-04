c121e3d5972dc30aae5772da849435538b2af458
JYKIM317 <augu202@naver.com>
2024-11-04 21:39:29 +0900

    refactor: RTMP Stream Connection 분리
    메인에서 socket을 전달하여 Stream을 관리하는 RTMPStream 클래스를
    작성하여 데이터와 이벤트를 관리하도록 리팩토링함.
    
    Co-authored-by: zzawang <hobinchoi0116@gmail.com>

--- mediaServer/main.ts
+++ mediaServer/main.ts
@@ -1,23 +1,9 @@
 import net from 'net';
-import crypto from 'crypto';
-import { handshake, HandshakeData } from '@core/rtmp/handshake';
+import RTMPStream from '@core/rtmp/stream';
 
 const server = net.createServer((socket) => {
-  const handshakeData: HandshakeData = {
-    C0: Buffer.alloc(0),
-    C1: Buffer.alloc(0),
-    C2: Buffer.alloc(0),
-    C1_TIMESTAMP: Buffer.alloc(0),
-    C1_RANDOM_BYTES: Buffer.alloc(0),
-    S1_TIMESTAMP: Buffer.alloc(4),
-    S1_RANDOM_BYTES: crypto.randomBytes(1528),
-  };
-
-  socket.on('data', (data) => {
-    handshake(socket, data, handshakeData);
-  });
-
-  socket.on('error', (_) => socket.end());
+  const rtmpStream = new RTMPStream(socket);
+  rtmpStream.run();
 });
 
 const PORT = 3000;
