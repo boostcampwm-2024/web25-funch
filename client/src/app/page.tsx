0aa141f122f9923dc5de5f56057deebcdd887d06
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-05 11:32:37 +0900

    setting: msw 설정
    - handler, browser 설정
    - 진입점에서 worker 시작
    - 서버 / 브라우저 분기처리
    - API 예시 작성

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Ping from './components/Ping';
+
 export default function Home() {
-  return <h1 className="bg-surface-brand-base font-light">FUNCH</h1>;
+  return (
+    <h1 className="bg-surface-brand-base font-light">
+      FUNCH
+      <Ping />
+    </h1>
+  );
 }
