1412706fc476964315578adb5f46b74f56f39fa8
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-05 21:51:18 +0900

    feature: Toolbar UI 구성
    - 펀치 로고 배치
    - 테마 컨트롤러 배치
    - 스튜디오 버튼 배치
    - 로그아웃 버튼 배치

---
+++ client/src/app/components/layout/LoginBtn.tsx
@@ -0,0 +1,9 @@
+const LoginBtn = () => {
+  return (
+    <div className="border-border-neutral-base text-content-static-white hover:bg-surface-neutral-strong flex items-center rounded-md border px-4 py-1">
+      <button className="btn btn-primary">로그아웃</button>
+    </div>
+  );
+};
+
+export default LoginBtn;
