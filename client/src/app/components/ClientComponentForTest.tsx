b517a1d925024792d88cd7ac9d81933a7ab0ce82
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-07 01:05:29 +0900

    feat: 서랍 상단 UI 팔로우 채널
    - pathname 별로 색상 변경
    - 팔로잉 채널 렌더링

--- client/src/app/components/ClientComponentForTest.tsx
+++ client/src/app/components/ClientComponentForTest.tsx
@@ -3,11 +3,7 @@
 import AccordionButton from './AccordionButton';
 
 const ClientComponentForTest = () => {
-  return (
-    <div>
-      <AccordionButton onExpand={() => console.log('expand!')} onCollapse={() => console.log('collapse!')} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ClientComponentForTest;
--- client/src/app/components/cabinet/CabinetContainer.tsx
+++ client/src/app/components/cabinet/CabinetContainer.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import CabinetLink from './CabinetLink';
-import CategoryLink from './CategoryLink';
-import FollowLink from './FollowLink';
+import RefreshSvg from '@components/svgs/RefreshSvg';
+import DownArrowSvg from '@components/svgs/DownArrowSvg';
+import FollowList from './FollowList';
 
 const CabinetContainer = () => {
   return (
@@ -13,7 +17,7 @@ const CabinetContainer = () => {
 
 const CategoryNavigator = () => {
   return (
-    <div className="funch-desktop:h-44 h-48 overflow-hidden">
+    <div className="funch-desktop:h-40 h-40 overflow-hidden">
       <CabinetLink link="category" />
       <CabinetLink link="follow" />
     </div>
@@ -21,9 +25,42 @@ const CategoryNavigator = () => {
 };
 
 const StreamerNavigator = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1200);
+
+  useEffect(() => {
+    console.log(isDesktop);
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1200);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="funch-desktop:w-3/4 border-t-bg-strong mx-auto h-full w-1/2 overflow-y-auto border-t">
-      <h2></h2>
+    <div className="funch-desktop:w-5/6 border-y-surface-neutral-base mx-auto w-1/2 overflow-hidden border-y-2 py-4">
+      {isDesktop ? <FollowDesktopHeader /> : <FollowNavHeader />}
+      <FollowList />
+    </div>
+  );
+};
+
+const FollowNavHeader = () => {
+  return (
+    <div className="text-content-neutral-strong w-full text-center">
+      <h2 className="funch-bold12">팔로우</h2>
+    </div>
+  );
+};
+
+const FollowDesktopHeader = () => {
+  return (
+    <div className="text-content-neutral-strong flex justify-between">
+      <h2 className="funch-bold14 funch-desktop:funch-bold16">팔로잉 채널</h2>
+      <section className="flex">
+        <RefreshSvg />
+        <DownArrowSvg />
+      </section>
     </div>
   );
 };
