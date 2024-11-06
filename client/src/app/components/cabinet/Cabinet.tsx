02f35959906c12850fb1027257fab483eee379f9
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-07 01:05:29 +0900

    feat: 서랍 상단 UI 팔로우 채널
    - pathname 별로 색상 변경
    - 팔로잉 채널 렌더링

--- client/src/app/components/cabinet/Cabinet.tsx
+++ client/src/app/components/cabinet/Cabinet.tsx
@@ -3,7 +3,7 @@ import CabinetContainer from './CabinetContainer';
 
 const Cabinet = () => {
   return (
-    <section className="funch-desktop:w-60 fixed left-0 top-0 h-full w-28 overflow-auto">
+    <section className="funch-desktop:w-60 fixed left-0 top-0 h-full w-24 overflow-auto pl-4">
       <CabinetContainer />
     </section>
   );
