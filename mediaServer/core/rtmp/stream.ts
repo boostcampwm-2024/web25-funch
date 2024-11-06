7d1d83805a821318afc49f2e10f9ab70a8457dcf
JYKIM317 <augu202@naver.com>
2024-11-07 04:03:09 +0900

    fix: RTMP 청크 파싱 문제 해결
    이전에 구현했던 RTMP 청크 파싱 로직이 정상적으로 파싱되지 않음을
    식별했고, 간헐적으로 오류가 발생해 해당 문제를 해결하였다.
    
    Co-authored-by: zzawang <hobinchoi0116@gmail.com>

--- mediaServer/core/rtmp/stream.ts
+++ mediaServer/core/rtmp/stream.ts
@@ -1,6 +1,6 @@
 import net from 'net';
 import crypto from 'crypto';
-import { separateRtmpChunk, RtmpChunck } from '@core/rtmp/packet';
+import { RtmpChunk, createRtmpChunk, parseBasicHeader, parseMessageHeader } from '@core/rtmp/packet';
 import { handshake, HandshakeData } from '@core/rtmp/handshake';
 import { connect } from '@core/rtmp/connect';
 import { createStream } from '@core/rtmp/createStream';
@@ -32,11 +32,11 @@ const COMMAND_AMF3 = 17;
 class RTMPStream {
   handshakeData: HandshakeData;
   isHandshakeDone: boolean;
-  isConnectDone: boolean;
-  isCreateStreamDone: boolean;
-  isPublishDone: boolean;
   streamKey?: string;
   streamCount: number;
+  chunkSize: number;
+  chunkMap: Map<number, RtmpChunk>;
+  workingChunk: RtmpChunk;
 
   constructor(private socket: net.Socket) {
     this.handshakeData = {
@@ -50,10 +50,10 @@ class RTMPStream {
     };
 
     this.isHandshakeDone = false;
-    this.isConnectDone = false;
-    this.isCreateStreamDone = false;
-    this.isPublishDone = false;
     this.streamCount = 0;
+    this.chunkSize = 128;
+    this.chunkMap = new Map();
+    this.workingChunk = createRtmpChunk();
   }
 
   run() {
@@ -66,37 +66,17 @@ class RTMPStream {
   dataEvent(data) {
     if (!this.isHandshakeDone) {
       this.isHandshakeDone = handshake(this.socket, data, this.handshakeData);
-    } else if (!this.isConnectDone) {
-      // TODO: 중복 개선
-      const chunks = separateRtmpChunk(data);
-      chunks.forEach((chunk) => {
-        const typeId = chunk.messageHeader.typeId;
-        this.handleMessage(typeId, chunk);
-      });
-    } else if (!this.isCreateStreamDone) {
-      const chunks = separateRtmpChunk(data);
-      chunks.forEach((chunk) => {
-        const typeId = chunk.messageHeader.typeId;
-        this.handleMessage(typeId, chunk);
-      });
-    } else if (!this.isPublishDone) {
-      const chunks = separateRtmpChunk(data);
-      chunks.forEach((chunk) => {
-        const typeId = chunk.messageHeader.typeId;
-        this.handleMessage(typeId, chunk);
-      });
     } else {
-      // TODO: Data Message (Metadata) 처리
-      const chunks = separateRtmpChunk(data);
-      console.log(chunks);
+      this.separateRtmpChunk(data);
     }
   }
 
   closeEvent() {
-    this.socket.destroy();
+    this.socket.end();
   }
 
   errorEvent(error) {
+    console.error(error);
     this.socket.destroy();
   }
 
@@ -104,7 +84,8 @@ class RTMPStream {
     this.socket.destroy();
   }
 
-  handleMessage(typeId: number, chunk: RtmpChunck) {
+  handleMessage() {
+    const typeId = this.workingChunk.messageHeader.typeId;
     switch (typeId) {
       case 1:
       case 2:
@@ -113,28 +94,47 @@ class RTMPStream {
       case 5:
       case 6:
         // TODO: 제어 메시지 처리
-        this.handleControlMessage(typeId, chunk);
+        this.handleControlMessage(typeId);
         break;
       case 17:
       case 20:
-        this.handleCommandMessage(typeId, chunk);
+        this.handleCommandMessage(typeId);
+        break;
+      case 19:
+      case 16:
+        const sharedObjMessage = decodeAMF(typeId, this.workingChunk.payload, 19, 16);
+        console.log("shared Object Message ", sharedObjMessage);
         break;
       default:
         // TODO: 데이터 메시지 처리
+        this.handleDataMessage(typeId);
         break;
     }
   }
 
-  handleControlMessage(typeId: number, chunk: RtmpChunck) {}
+  handleControlMessage(typeId: number) {
+    switch(typeId) {
+      case SET_CHUNK_SIZE:
+        this.chunkSize = this.workingChunk.payload.readUInt32BE();
+        break;
+      case ABORT_MESSAGE:
+        break;
+      case ACK:
+        break;
+      case USER_CONTROL_MESSAGE:
+        break;
+      case WINDOW_ACK_SIZE:
+        break;
+      case SET_PEER_BANDWIDTH:
+        break;
+    }
+  }
 
-  handleCommandMessage(typeId: number, chunk: RtmpChunck) {
-    const AMF0 = 20;
-    const AMF3 = 17;
-    const decodedPayload = decodeAMF(typeId, chunk.payload, AMF0, AMF3);
-
+  handleCommandMessage(typeId: number) {
+    const decodedPayload = decodeAMF(typeId, this.workingChunk.payload, COMMAND_AMF0, COMMAND_AMF3);
     switch (decodedPayload.cmd) {
       case 'connect':
-        this.isConnectDone = connect(this.socket);
+        connect(this.socket);
         break;
       case 'releaseStream':
       case 'FCPublish':
@@ -143,17 +143,115 @@ class RTMPStream {
       case 'createStream':
         // TODO: DB 저장된 streamKey와 비교하여 등록된 스트림 key가 아니라면 error 이벤트 발생 함수
         this.streamCount++;
-        this.isCreateStreamDone = createStream(this.socket, this.streamCount);
+        createStream(this.socket, this.streamCount);
         break;
       case 'publish':
-        this.isPublishDone = publish(this.socket);
+        publish(this.socket);
         break;
       default:
         break;
     }
   }
 
-  handleDataMessage(typeId: number, chunk: RtmpChunck) {}
+  async handleDataMessage(typeId: number) {  
+    switch (typeId) {
+      case VIDEO_MESSAGE:
+        break;
+      case AUDIO_MESSAGE:
+        break;
+    }
+  }
+
+  separateRtmpChunk(data: Buffer) {
+    let offset = 0;
+    let currentChunkState = 0;
+
+    if(this.workingChunk.extraBytes > 0) {
+      this.workingChunk.payload = Buffer.concat([this.workingChunk.payload, data.subarray(offset, this.workingChunk.extraBytes)]);
+      offset += this.workingChunk.extraBytes;
+      this.workingChunk.extraBytes = 0;
+    }
+
+    while (offset < data.length) {
+      switch (currentChunkState % 4) {
+        case BASIC_HEADER_STATE:
+          const basicHeaderBuf = data.subarray(offset, offset + 1)[0];
+          const basicHeaderType = basicHeaderBuf & 0x3f;
+          let basicHeaderBytes = 1;
+          if (basicHeaderType === 0) {
+            basicHeaderBytes = 2;
+          } else if (basicHeaderType === 1) {
+            basicHeaderBytes = 3;
+          }
+          
+          const { chunkType, chunkStreamId } = parseBasicHeader(data.subarray(offset, offset + basicHeaderBytes));
+          if (!this.chunkMap.has(chunkStreamId)) {
+            this.workingChunk = createRtmpChunk(chunkType, chunkStreamId);
+            this.chunkMap.set(chunkStreamId, this.workingChunk);
+          } else {
+            this.workingChunk = this.chunkMap.get(chunkStreamId)!;
+            this.workingChunk.basicHeader.chunkType = chunkType;
+          }
+
+          offset += basicHeaderBytes;
+          currentChunkState += 1;
+          break;
+          
+        case MESSAGE_HEADER_STATE:
+          let messageHeaderBytes = 0;
+          if (this.workingChunk.basicHeader.chunkType === 0) {
+            messageHeaderBytes = 11;
+          } else if (this.workingChunk.basicHeader.chunkType === 1) {
+            messageHeaderBytes = 7;
+          } else if (this.workingChunk.basicHeader.chunkType === 2) {
+            messageHeaderBytes = 3;
+          } else if (this.workingChunk.basicHeader.chunkType === 3) {
+            currentChunkState += 1;
+            break;
+          }
+
+          this.workingChunk.messageHeader = parseMessageHeader(
+            this.workingChunk.messageHeader,
+            this.workingChunk.basicHeader.chunkType,
+            data.subarray(offset, offset + messageHeaderBytes),
+          );
+
+          offset += messageHeaderBytes;
+          currentChunkState += 1;
+          break;
+
+        case EXTENDED_TIMESTAMP_STATE:
+          const EXTENDED_TIMESTAMP_FLAG = this.workingChunk.messageHeader.timestamp === 0xffffff;
+          if (EXTENDED_TIMESTAMP_FLAG) {
+            this.workingChunk.extendedTimestamp = data.subarray(offset, offset + 4).readUInt32BE();
+            offset += 4;
+          }
+
+          currentChunkState += 1;
+          break;
+
+        case PAYLOAD_STATE:
+          const messageLength = this.workingChunk.messageHeader.messageLength;
+          const availablePayloadLength = Math.min(messageLength - this.workingChunk.payload.length, this.chunkSize);
+          const minimumValid = Math.min(availablePayloadLength, data.subarray(offset).length);
+
+          if(((this.workingChunk.messageHeader.messageLength - this.workingChunk.payload.length) > this.chunkSize) && (data.subarray(offset).length < this.chunkSize)) {
+            this.workingChunk.extraBytes = this.chunkSize  - minimumValid;
+          }
+
+          this.workingChunk.payload = Buffer.concat([this.workingChunk.payload, data.subarray(offset, offset + minimumValid)]);
+          offset += minimumValid;
+        
+          currentChunkState += 1;
+          if(this.workingChunk.payload.length === this.workingChunk.messageHeader.messageLength) {
+            this.handleMessage();
+            this.workingChunk.payload = Buffer.alloc(0);
+          }
+
+          break;
+      }
+    }
+  }
 }
 
 export default RTMPStream;
