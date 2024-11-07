2ad96974333116cd9365d89705b4390d36ec69cc
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-07 14:55:47 +0900

    feat: 서랍UI 팔로잉 채널 스트리머 정보 UI 구현

--- client/src/app/components/cabinet/Cabinet.tsx
+++ client/src/app/components/cabinet/Cabinet.tsx
@@ -3,7 +3,7 @@ import CabinetContainer from './CabinetContainer';
 
 const Cabinet = () => {
   return (
-    <section className="funch-desktop:w-60 fixed left-0 top-0 h-full w-24 overflow-auto pl-4">
+    <section className="funch-desktop:w-60 fixed left-0 top-0 z-50 h-full w-24 overflow-auto">
       <CabinetContainer />
     </section>
   );
