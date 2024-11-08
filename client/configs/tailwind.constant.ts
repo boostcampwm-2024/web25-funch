ff453f458ff009e0bdd88ce057cb75bb649fc47b
Wille Lee <1992season@gmail.com>
2024-11-09 03:37:08 +0900

    feat: 검색창 너비 반응형 - 검색창 너비에 대한 변수를 설정하고 화면 너비에 따라 달리 사용

---
+++ client/configs/tailwind.constant.ts
@@ -0,0 +1,11 @@
+export const LAYOUT_MIN_WIDTH = '58.125rem';
+export const HEADER_HEIGHT = '5rem'; // 80px
+
+export const HOME_MIN_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`;
+
+export const LIVE_X_GAP = '0.875rem'; // 14px
+export const LIVE_WIDTH = `calc(33.332% - (${LIVE_X_GAP} * 2 / 3))`;
+export const LIVE_ASPECT_RATIO = '56.25%'; // 16:9
+
+export const SEARCH_WIDTH = 'calc(100% - 41.25rem)'; // 660px = 41.25rem
+export const SEARCH_WIDTH_WIDE = 'calc(100% - 58.125rem)'; // 930px = 58.125rem
