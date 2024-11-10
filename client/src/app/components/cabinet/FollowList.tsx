4b62f6f7e16b1f14f9fbfc313ca1a6efbb37b43c
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-10 20:47:37 +0900

    feat: 팔로잉 채널 UI - hover 단위를 group에서 peer로 변경 -

--- client/src/app/components/cabinet/FollowList.tsx
+++ client/src/app/components/cabinet/FollowList.tsx
@@ -39,8 +39,8 @@ const Follow = ({ follow, isDesktop }: { follow: Live; isDesktop: boolean }) => 
   return (
     <>
       {isDesktop ? (
-        <div className="group relative">
-          <div className="border-neutral-weak funch-desktop:justify-start hover:bg-border-neutral-base group flex w-full items-center rounded-md py-3 pl-2">
+        <div className="relative">
+          <div className="border-neutral-weak funch-desktop:justify-start hover:bg-surface-neutral-strong peer flex w-full items-center rounded-md py-3 pl-2">
             <div className="relative flex flex-1">
               <img
                 src={follow.streamer?.profileImage}
@@ -55,22 +55,24 @@ const Follow = ({ follow, isDesktop }: { follow: Live; isDesktop: boolean }) => 
                 <em className="text-content-red-base funch-bold16 flex items-center pr-2">{'· ' + follow.viewers}</em>
               </section>
             </div>
-            <div className="text-content-neutral-primary border-neutral-weak bg-border-neutral-base absolute left-52 top-0 z-50 flex h-20 w-60 items-center rounded-md p-4 opacity-0 group-hover:opacity-100">
-              {follow.title}
-            </div>
+          </div>
+          <div className="text-content-neutral-primary border-neutral-weak bg-surface-neutral-strong absolute left-52 top-0 z-50 flex h-20 w-60 items-center rounded-md p-4 opacity-0 peer-hover:opacity-100">
+            {follow.title}
           </div>
         </div>
       ) : (
-        <div
-          className={`border-neutral-weak funch-desktop:justify-start group relative flex w-full items-center rounded-md py-3 pl-2 ${follow.isStreaming && 'pl-[5px]'}`}
-        >
-          <img
-            src={follow.streamer?.profileImage}
-            alt={follow.title}
-            className={`border-border-brand-strong box-content h-8 w-8 rounded-full ${follow.isStreaming && 'border-2 hover:m-[-2px] hover:border-4'}`}
-          />
+        <div className="relative">
+          <div
+            className={`border-neutral-weak funch-desktop:justify-start peer relative flex w-full items-center rounded-md py-3 pl-2 ${follow.isStreaming && 'pl-[5px]'}`}
+          >
+            <img
+              src={follow.streamer?.profileImage}
+              alt={follow.title}
+              className={`border-border-brand-strong box-content h-8 w-8 rounded-full ${follow.isStreaming && 'border-2 hover:m-[-2px] hover:border-4'}`}
+            />
+          </div>
           {follow.isStreaming && (
-            <div className="bg-surface-neutral-base z-100 absolute left-14 top-3 flex h-32 w-52 flex-col rounded-md p-3 opacity-0 group-hover:opacity-100">
+            <div className="bg-surface-neutral-base z-100 absolute left-14 top-3 flex h-32 w-52 flex-col rounded-md p-3 opacity-0 peer-hover:opacity-100">
               <div className="flex h-1/4 w-full items-center gap-2 truncate">
                 <div className="text-content-brand-strong funch-bold16">{follow.streamer.name}</div>
                 <div className="funch-bold14 border-border-neutral-base rounded-md border-2 p-[3px]">
