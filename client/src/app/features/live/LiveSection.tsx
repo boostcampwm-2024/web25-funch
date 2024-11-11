45d38c182c08253cea78883e8b98601c7f13e7f1
Wille Lee <1992season@gmail.com>
2024-11-11 17:03:12 +0900

    feat: live section, live provider - 라이브 섹션 및 라이브 프로바이더 프로토타입 작성

---
+++ client/src/app/features/live/LiveSection.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import LiveProvider from '@providers/LiveProvider';
+import { type ReactNode, useRef, useState } from 'react';
+import NoLiveContent from './NoLiveContent';
+
+const LiveSection = () => {
+  const ref = useRef<HTMLVideoElement>(null);
+  const pathname = usePathname();
+
+  const play = () => {
+    if (ref.current) {
+      ref.current.play();
+    }
+  };
+
+  if (pathname.split('/')[1] !== 'lives') return null;
+
+  return (
+    <LiveProvider>
+      <section>
+        <LiveController>
+          {({ isStreaming }) =>
+            isStreaming ? (
+              <div className="pl-60" id="test-div">
+                <video
+                  ref={ref}
+                  controls
+                  controlsList="nodownload"
+                  className="relative"
+                  id="test-video"
+                  src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+                  width={600}
+                >
+                  <p className="text-red-5 absolute left-0 top-0">hihih</p>
+                </video>
+                <PlayButton play={play} />
+              </div>
+            ) : (
+              <NoLiveContent />
+            )
+          }
+        </LiveController>
+      </section>
+    </LiveProvider>
+  );
+};
+
+const PlayButton = ({ play }: { play: () => void }) => {
+  return <button onClick={play}>재생</button>;
+};
+
+const LiveController = ({ children }: { children: (args: { isStreaming: boolean }) => ReactNode }) => {
+  const [isStreaming, setIsStreaming] = useState(true);
+
+  return children({ isStreaming });
+};
+
+export default LiveSection;
