4968799984e4223fc8197f80d1733daebebec407
Wille Lee <1992season@gmail.com>
2024-11-11 19:31:26 +0900

    feat: live controller 분리 진행 중 - live controller 분리 진행 중

---
+++ client/src/app/features/live/Live.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+type Props = {};
+
+const LiveController = (props: Props) => {
+  return <div>Live</div>;
+};
+
+const Live = Object.assign(LiveController, {});
+
+export default Live;
