cd1ab88732863f0ea1227b0158bdd0ba37ea9952
Wille Lee <1992season@gmail.com>
2024-11-12 17:11:56 +0900

    feat: live section live info 컴포넌트 UI - LiveInfo의 UI를 작성

--- client/src/app/__mocks__/lives.ts
+++ client/src/app/__mocks__/lives.ts
@@ -4,12 +4,12 @@ export const mockedLives: Live[] = [
   {
     id: '1',
     thumbnail: 'https://via.placeholder.com/150',
-    viewers: 100,
-    title: '[V리그] 생중계 GS칼텍스 vs IBK기업은행 #watchparty (IBK 응원)',
+    viewers: 10034002,
+    title: '[V리그] 생중계 GS칼텍스 vs IBK기업은행 #watchparty (IBK 응원) #V리그 #V리그생중계',
     category: '카테고리',
-    tags: ['태그1', '태그2', '태그2', '태그2', '태그2', '태그2'],
+    tags: ['경제', '수다', '글로벌', '태그2', '태그2', '태그2'],
     streamer: {
-      name: '스트리머 이름',
+      name: '슈카월드',
       profileImage: 'https://via.placeholder.com/150',
     },
   },
