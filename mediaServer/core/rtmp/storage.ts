ab7dc9a524aee4e0868eee47a6169ed870bec0b2
JYKIM317 <augu202@naver.com>
2024-11-13 03:51:38 +0900

    feat: Object Storage 저장 구현
    NCP Object Storage 사용을 위해 AWS sdk를 이용한 Object Storage 저장
    로직을 구현하였다.
    
    Co-authored-by: zzawang <hobinchoi0116@gmail.com>

---
+++ mediaServer/core/rtmp/storage.ts
@@ -0,0 +1,35 @@
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+
+const END_POINT = new AWS.Endpoint('https://kr.object.ncloudstorage.com');
+const REGION = 'kr-standard';
+const ACCESS_KEY = process.env.ACCESS_KEY as string;
+const SECRET_KEY = process.env.SECRET_KEY as string;
+const BUCKET_NAME = 'media-storage';
+
+const S3 = new AWS.S3({
+  endpoint: END_POINT,
+  region: REGION,
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_KEY,
+  },
+});
+
+async function uploadData(path, filePath) {
+  try {
+    await S3.putObject({
+      Bucket: BUCKET_NAME,
+      Key: path,
+      ACL: 'public-read',
+      Body: fs.createReadStream(filePath),
+    }).promise();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export { uploadData };
