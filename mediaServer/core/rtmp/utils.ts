139cd975cb2bba1f11045f100708a4d70d6a0011
JYKIM317 <augu202@naver.com>
2024-11-12 21:03:02 +0900

    fix: AMF 디코딩 에러 처리
    AMF 디코딩 중 발생할 수 있는 에러로 인해 프로세스가 종료되는 것을
    방지하고자 try-catch 문을 추가하였다.
    
    Co-authored-by: zzawang <hobinchoi0116@gmail.com>

--- mediaServer/core/rtmp/utils.ts
+++ mediaServer/core/rtmp/utils.ts
@@ -1,10 +1,14 @@
 import { decodeAmf0Cmd, decodeAmf3Cmd } from 'node-amfutils';
 
 function decodeAMF(typeId: number, paylod: Buffer, amf0Num: number, amf3Num: number) {
-  if (typeId === amf0Num) {
-    return decodeAmf0Cmd(paylod);
-  } else if (typeId === amf3Num) {
-    return decodeAmf3Cmd(paylod);
+  try {
+    if (typeId === amf0Num) {
+      return decodeAmf0Cmd(paylod);
+    } else if (typeId === amf3Num) {
+      return decodeAmf3Cmd(paylod);
+    }
+  } catch (e) {
+    console.log(typeId, JSON.stringify(paylod), e);
   }
 }
 
