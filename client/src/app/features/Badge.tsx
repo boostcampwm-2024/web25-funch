189ad59e62f41488d4d1f52d6c598776bbe8897b
Wille Lee <1992season@gmail.com>
2024-11-12 17:11:56 +0900

    feat: live section live info 컴포넌트 UI - LiveInfo의 UI를 작성

--- client/src/app/features/Badge.tsx
+++ client/src/app/features/Badge.tsx
@@ -1,8 +1,30 @@
+import clsx from 'clsx';
 import { type PropsWithChildren } from 'react';
 
-const Badge = ({ children }: PropsWithChildren) => {
+const BADGE_COMPONENT_TYPE = {
+  DEFAULT: 'DEFAULT' as const,
+  OUTLINE: 'OUTLINE' as const,
+};
+
+type BadgeComponentType = keyof typeof BADGE_COMPONENT_TYPE;
+
+type Props = PropsWithChildren<{
+  componentType?: BadgeComponentType;
+}>;
+
+const Badge = ({ children, componentType = 'DEFAULT' }: Props) => {
   return (
-    <span className="bg-surface-neutral-weak text-content-neutral-strong funch-bold12 cursor-default rounded-md px-1.5 py-1">
+    <span
+      className={clsx(
+        'funch-bold10 cursor-default rounded-md px-1 py-0.5',
+
+        {
+          'bg-surface-neutral-weak text-content-neutral-strong': componentType === BADGE_COMPONENT_TYPE.DEFAULT,
+          'text-content-neutral-base border-border-neutral-base border border-solid bg-transparent':
+            componentType === BADGE_COMPONENT_TYPE.OUTLINE,
+        },
+      )}
+    >
       {children}
     </span>
   );
