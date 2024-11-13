3f2cc15f37024cb34c17595a970f68cbdb3e4c33
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-13 10:54:28 +0900

    feat: NoLiveContent UI

--- client/src/app/features/live/NoLiveContent.tsx
+++ client/src/app/features/live/NoLiveContent.tsx
@@ -1,21 +1,25 @@
 'use client';
 
-import Button from '@components/Button';
+import Link from 'next/link';
 import NotifySvg from '@components/svgs/NotifySvg';
 
 const NoLiveContent = () => {
   return (
-    <div className="">
+    <div className="min-h-home flex w-full flex-col items-center justify-center">
       <NotifySvg />
-      <strong>존재하지 않는 채널입니다.</strong>
-      <p>
+      <strong className="funch-bold20 text-surface-neutral-inverse my-2">존재하지 않는 채널입니다.</strong>
+      <p className="funch-medium14 mb-4 text-center">
         지금 입력하신 주소의 페이지는
         <br />
         사라졌거나 다른 페이지로 변경되었습니다.
         <br />
         주소를 다시 확인해주세요.
       </p>
-      <Button>다른 방송 보러가기</Button>
+      <Link href="/" title="펀치 홈으로">
+        <button className="bg-surface-static-warmgray hover:bg-surface-static-coolgray text-content-static-white funch-bold16 rounded-3xl px-4 py-2">
+          다른 방송 보러가기
+        </button>
+      </Link>
     </div>
   );
 };
