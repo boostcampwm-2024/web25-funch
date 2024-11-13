9b963bb976a6089f9ce1bd26b50f2d0c2c80f143
JYKIM317 <augu202@naver.com>
2024-11-13 18:14:01 +0900

    feat: 마스터 플레이리스트 생성 및 코드 분리
    feat: 마스터 플레이리스트 생성을 위해 createMasterPlaylist 함수를
    작성하였다.
    refactor: ffmpeg과 fs.watch 및 object storage 업드 로직을 외부 파일로
    분리하였다.
    
    Co-authored-by: zzawang <hobinchoi0116@gmail.com>

---
+++ mediaServer/core/rtmp/ffmpeg.ts
@@ -0,0 +1,80 @@
+import fs from 'fs';
+import ffmpegPath from '@ffmpeg-installer/ffmpeg';
+import ffprobePath from '@ffprobe-installer/ffprobe';
+import ffmpeg from 'fluent-ffmpeg';
+import { PassThrough } from 'stream';
+
+ffmpeg.setFfmpegPath(ffmpegPath.path);
+ffmpeg.setFfprobePath(ffprobePath.path);
+
+const defaultOutputOptions = [
+  '-map 0:v',
+  '-map 0:a',
+  '-hls_time 2',
+  '-hls_list_size 3',
+  '-hls_segment_type fmp4',
+  '-hls_flags delete_segments+split_by_time+independent_segments+omit_endlist',
+  '-preset veryfast',
+];
+
+// .outputOptions(['-master_pl_name master.m3u8'])
+
+function initializeFFMepg(ffmpegInputStream: PassThrough, storagePath: string) {
+  return ffmpeg()
+    .input(ffmpegInputStream)
+    .inputOptions(['-re'])
+    .inputFormat('flv')
+
+    .output(`${storagePath}/chunklist_1080p_.m3u8`)
+    .videoCodec('libx264')
+    .audioCodec('aac')
+    .size('1920x1080')
+    .videoBitrate('5000k')
+    .audioBitrate('192k')
+    .outputOptions([...defaultOutputOptions, '-hls_fmp4_init_filename chunkList_1080p_0_0.mp4'])
+
+    .output(`${storagePath}/chunklist_720p_.m3u8`)
+    .videoCodec('libx264')
+    .audioCodec('aac')
+    .size('1280x720')
+    .videoBitrate('3000k')
+    .audioBitrate('128k')
+    .outputOptions([...defaultOutputOptions, '-hls_fmp4_init_filename chunkList_720p_0_0.mp4'])
+
+    .output(`${storagePath}/chunklist_480p_.m3u8`)
+    .videoCodec('libx264')
+    .audioCodec('aac')
+    .size('854x480')
+    .videoBitrate('1500k')
+    .audioBitrate('96k')
+    .outputOptions([...defaultOutputOptions, '-hls_fmp4_init_filename chunkList_480p_0_0.mp4'])
+
+    .on('end', (err, stdout, stderr) => {
+      console.log('Finished processing!', err, stdout, stderr);
+    })
+    .on('close', (code) => {
+      console.log(`FFmpeg process exited with code ${code}`);
+    })
+    .on('error', (code) => {
+      console.log(`FFmpeg process error with code ${code}`);
+    });
+}
+
+function createMasterPlaylist(storagePath) {
+  const masterPlaylist = `
+#EXTM3U
+#EXT-X-VERSION:7
+
+#EXT-X-STREAM-INF:BANDWIDTH=5711200,RESOLUTION=1920x1080,CODECS="avc1.64002a,mp4a.40.2"
+chunklist_1080p_.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=3440800,RESOLUTION=1280x720,CODECS="avc1.640020,mp4a.40.2"
+chunklist_720p_.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1755600,RESOLUTION=854x480,CODECS="avc1.64001f,mp4a.40.2"
+chunklist_480p_.m3u8
+`.trim();
+
+  const fileStream = fs.createWriteStream(`${storagePath}/master_playlist.m3u8`);
+  fileStream.write(masterPlaylist, () => fileStream.end());
+}
+
+export { initializeFFMepg, createMasterPlaylist };
