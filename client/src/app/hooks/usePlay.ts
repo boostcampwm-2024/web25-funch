a32e1694e04a6b7890277b26b63c31a8754fb21b
Wille Lee <1992season@gmail.com>
2024-11-14 11:38:55 +0900

    refactor: 비디오 컨트롤러 파일 분리 - 비디오 컨트롤러 부분을 라이브 파일로부터 분리했다.

--- client/src/app/hooks/usePlay.ts
+++ client/src/app/hooks/usePlay.ts
@@ -1,17 +1,35 @@
-import { type RefObject, useState } from 'react';
+import { type RefObject, useEffect, useState } from 'react';
 
 const usePlay = (ref: RefObject<HTMLVideoElement>) => {
   const [isPlay, setIsPlay] = useState(true);
 
   const togglePlay = () => {
-    if (ref.current && ref.current.paused) {
+    if (!ref.current) return;
+    if (ref.current.paused) {
       ref.current.play();
-      setIsPlay(true);
     } else {
-      ref.current?.pause();
-      setIsPlay(false);
+      ref.current.pause();
     }
   };
+
+  useEffect(() => {
+    const handlePlay = () => {
+      setIsPlay(true);
+    };
+    const handlePause = () => {
+      setIsPlay(false);
+    };
+    if (ref.current) {
+      ref.current.addEventListener('play', handlePlay);
+      ref.current.addEventListener('pause', handlePause);
+    }
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener('play', handlePlay);
+        ref.current.removeEventListener('pause', handlePause);
+      }
+    };
+  }, []);
 
   return { isPlay, togglePlay };
 };
