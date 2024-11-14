860da09cedf418b1eee5d3a7dafc3c74ae93ca29
Wille Lee <1992season@gmail.com>
2024-11-14 10:34:55 +0900

    fix: usePip 내 useEffect 추가 - 웹에서 제공하는 pip 종료 버튼을 핸들링하기 위해 enterpictureinpicture, leavepictureinpicture 이벤트를 핸들링하는 이팩트를 usePip에 추가

--- client/src/app/hooks/usePip.ts
+++ client/src/app/hooks/usePip.ts
@@ -1,4 +1,4 @@
-import { type RefObject, useState } from 'react';
+import { type RefObject, useEffect, useState } from 'react';
 
 const usePip = (ref: RefObject<HTMLVideoElement>) => {
   const [isPip, setIsPip] = useState(false);
@@ -6,14 +6,35 @@ const usePip = (ref: RefObject<HTMLVideoElement>) => {
   const togglePip = () => {
     if (document.pictureInPictureElement) {
       document.exitPictureInPicture();
-      setIsPip(false);
     } else {
       if (ref.current && ref.current.requestPictureInPicture) {
         ref.current.requestPictureInPicture();
-        setIsPip(true);
       }
     }
   };
+
+  useEffect(() => {
+    const handleEnterPip = () => {
+      if (document.pictureInPictureElement) {
+        setIsPip(true);
+      }
+    };
+    const handleLeavePip = () => {
+      if (!document.pictureInPictureElement) {
+        setIsPip(false);
+      }
+    };
+    if (ref.current) {
+      ref.current.addEventListener('enterpictureinpicture', handleEnterPip);
+      ref.current.addEventListener('leavepictureinpicture', handleLeavePip);
+    }
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener('enterpictureinpicture', handleEnterPip);
+        ref.current.removeEventListener('leavepictureinpicture', handleLeavePip);
+      }
+    };
+  }, []);
 
   return { isPip, togglePip };
 };
