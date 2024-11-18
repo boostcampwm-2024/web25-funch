6631753c32852d5326a7119dc72e9cb0b7ec3943
최호빈 <hobinchoi0116@gmail.com>
2024-11-19 03:42:33 +0900

    feat: 전체 Member 조회 API 구현
    - MemberController를 생성하고 전체 Member를 조회하는 API를 구현하였다.
    - MemberController를 MemberModule에 등록하였다.
    - MemberModule을 사용할 수 있도록 AppModule에 import시켰다.

--- applicationServer/src/member/member.module.ts
+++ applicationServer/src/member/member.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@src/database/database.module';
 import { memberProviders } from '@member/member.providers';
+import { MemberController } from '@member/member.controller';
 import { MemberService } from '@member/member.service';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [MemberController],
   providers: [...memberProviders, MemberService],
+  exports: [MemberService],
 })
 export class MemberModule {}
