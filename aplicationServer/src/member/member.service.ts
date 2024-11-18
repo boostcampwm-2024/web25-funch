7308f427c08139e252c4f6f6a1f74822f3d03557
최호빈 <hobinchoi0116@gmail.com>
2024-11-19 02:31:41 +0900

    feat: Member 모듈 생성
    - Member 도메인과 관련된 module, provider, service를 생성하였다.

---
+++ aplicationServer/src/member/member.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Member } from '@member/member.entity';
+import { MEMBER_REPOSITORY } from '@src/constants';
+
+@Injectable()
+class MemberService {
+  constructor(
+    @Inject(MEMBER_REPOSITORY)
+    private memberRepository: Repository<Member>,
+  ) {}
+
+  async findMembers(): Promise<Member[]> {
+    return this.memberRepository.find();
+  }
+}
+
+export { MemberService };
