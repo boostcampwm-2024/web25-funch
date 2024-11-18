2aae21beec08fdce0a10623e4a51781b562b5ba4
Wille Lee <1992season@gmail.com>
2024-11-19 02:29:11 +0900

    chore: /api/:path* rewrite 로직 추가 - /api/:path*에 대한 요청을 production인 경우 https://api.example.com/:path\*로 rewrite하도록 next.config.mjs 임시 수정

--- client/src/__test__/msw.test.ts
+++ client/src/__test__/msw.test.ts
@@ -3,6 +3,11 @@ import { mockedUsers } from '@mocks/users';
 import { describe, expect, test } from 'vitest';
 
 describe('msw handlers', () => {
+  test('should return pong', async () => {
+    const response = await fetch('/api/ping');
+    const data = await response.json();
+    expect(data).toBe('pong');
+  });
   test('should return mocked broadcasts', async () => {
     const response = await fetch('/api/broadcasts');
     const data = await response.json();
