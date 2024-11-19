aed465ca56bb160d0983acb9057d60c615411a8e
JYKIM317 <augu202@naver.com>
2024-11-20 03:11:34 +0900

    refactor: 전체 방송 및 추천 방송 반환 API 리팩토링
    기존 방송 정보만 반환하던 로직에서 방송 정보와 스트리머의 정보를 함께
    전달할 수 있도록 Interceptor를 추가하였습니다.
    다만 이후 방송 정보에 스트리머 정보의 일부를 포함할지 논의가 필요할 것
    같습니다.

--- applicationServer/src/live/live.module.ts
+++ applicationServer/src/live/live.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LiveService } from '@live/live.service';
 import { LiveController } from '@live/live.controller';
+import { MemberModule } from '@src/member/member.module';
 
 @Module({
+  imports: [MemberModule],
   controllers: [LiveController],
   providers: [LiveService],
 })
