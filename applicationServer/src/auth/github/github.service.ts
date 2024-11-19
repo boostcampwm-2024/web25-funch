5c39be964f00c01c9b673622c0bd8ac68defb5be
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 01:25:25 +0900

    feat: github 로그인 callback 구현
    - 사용자가 github 로그인을 통해 제공한 사용자 정보를 바탕으로 회원가입/로그인 로직 초반을 구현하였다.

---
+++ applicationServer/src/auth/github/github.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
+import { ACCESS_TOKEN_URL, APPLICATION_JSON, RESOURCE_URL, AUTHORIZATION_TOKEN } from '@src/constants';
+
+@Injectable()
+class GithubAuthService {
+  constructor() {}
+
+  async getAccessToken(code: string): Promise<string> {
+    const request = {
+      code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRETS,
+    };
+    const response = await axios.post(ACCESS_TOKEN_URL, request, {
+      headers: {
+        accept: APPLICATION_JSON,
+      },
+    });
+
+    if (response.data.error) {
+      throw new HttpException('Github AccessToken 가져오기 실패', HttpStatus.UNAUTHORIZED);
+    }
+    return response.data.access_token;
+  }
+
+  async getUserInfo(accessToken: string) {
+    const { data } = await axios.get(RESOURCE_URL, {
+      headers: {
+        Authorization: AUTHORIZATION_TOKEN(accessToken),
+      },
+    });
+
+    return data;
+  }
+}
+
+export { GithubAuthService };
