c1e449b6a4dcc5528842128f08a4894030ecbb80
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-19 16:40:39 +0900

    feat:StudioBadge

--- client/src/app/studio/StudioBadge.tsx
+++ client/src/app/studio/StudioBadge.tsx
@@ -1,12 +1,24 @@
 import DeleteBadgeSvg from '@components/svgs/DeleteBadgeSvg';
 import { PropsWithChildren } from 'react';
+import clsx from 'clsx';
 
-const StudioBadge = ({ children }: PropsWithChildren) => {
+type StudioBadgeProps = {
+  handler?: () => void;
+};
+
+const StudioBadge = ({ children, handler }: PropsWithChildren & StudioBadgeProps) => {
   return (
-    <span className="funch-medium12 border-1 bg-surface-neutral-base border-border-neutral-weak text-content-neutral-weak flex h-4 items-center justify-center gap-1 rounded-full border-[1px] px-2 py-1">
+    <div
+      className={clsx(
+        'funch-medium14 border-1 bg-surface-neutral-base border-border-neutral-weak text-content-neutral-weak cursor-default',
+        'hover:bg-surface-neutral-strong inline-flex h-4 items-center justify-center gap-1 rounded-full border-[1px] px-3 py-3',
+      )}
+    >
       {children}
-      <DeleteBadgeSvg />
-    </span>
+      <button onClick={handler}>
+        <DeleteBadgeSvg />
+      </button>
+    </div>
   );
 };
 
