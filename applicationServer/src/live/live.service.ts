c1970083e9d384353e9604f93423ebd71a4b7335
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-11-19 17:48:12 +0900

    style: 프리티어 적용으로 인한 코드 변경
    제목이 곧 내용!

--- applicationServer/src/live/live.service.ts
+++ applicationServer/src/live/live.service.ts
@@ -1,19 +1,21 @@
-import { Injectable, HttpException, HttpStatus} from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Live } from '@live/entities/live.entity';
-import { Playlist } from '@src/types'
+import { Broadcast, Playlist } from '@src/types';
 
 @Injectable()
 export class LiveService {
-  live: Live
+  live: Live;
   constructor() {
     this.live = Live.getInstance();
   }
 
   responsePlaylistUrl(broadcastId) {
-    const createMultivariantPlaylistUrl = (id) =>  `https://kr.object.ncloudstorage.com/media-storage/${id}/master_playlist.m3u8`;
+    const createMultivariantPlaylistUrl = (id) =>
+      `https://kr.object.ncloudstorage.com/media-storage/${id}/master_playlist.m3u8`;
+
+    if (!this.live.data.has(broadcastId)) throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     
-    if(this.live.data.has(broadcastId)) {
-      const playlist: Playlist = { url: createMultivariantPlaylistUrl(broadcastId) }
+    const playlist: Playlist = { url: createMultivariantPlaylistUrl(broadcastId) };
       return playlist;
     } else {
       throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
