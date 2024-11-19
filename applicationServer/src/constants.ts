ff299bd56d7c659abb2cc884557d018ce44b9ec5
최호빈 <hobinchoi0116@gmail.com>
2024-11-19 17:56:08 +0900

    refactor: 매직넘버 및 상수들을 파일로 분리
    - url 및 형식을 갖춰야 하는 문자열들을 상수로 생성하여 파일로 분리하였다.

--- applicationServer/src/constants.ts
+++ applicationServer/src/constants.ts
@@ -1,9 +1,14 @@
 // member.providers.ts
 export const MEMBER_REPOSITORY = 'MEMBER_REPOSITORY';
 export const DATA_SOURCE = 'DATA_SOURCE';
-
 // live.controller.ts
 export const SUGGEST_LIVE_COUNT = 10;
-
 // live.service.ts
 export const NOTIFY_LIVE_DATA_INTERVAL_TIME = 30000;
+// github.controller.ts
+export const GITHUB_LOGIN_ID = (id: string) => `Github@${id}`;
+// github.service.ts
+export const ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+export const APPLICATION_JSON = 'application/json';
+export const RESOURCE_URL = 'https://api.github.com/user';
+export const AUTHORIZATION_TOKEN = (accessToken: string) => `token ${accessToken}`;
