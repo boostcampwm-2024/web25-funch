0456fdb66bcd62a600047ef6146c71904f6cda56
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-11-19 15:40:06 +0900

    feat: 전역 라이브 정보 객체 생성
    라이브 정보 확인 및 등록을 위한 라이브 객체를 작성하였다.

---
+++ applicationServer/src/live/entities/live.entity.ts
@@ -0,0 +1,15 @@
+import { Broadcast } from '@src/types'
+
+class Live {
+    public data: Map<string, Broadcast>;
+    private static instance: Live;
+    private constructor() {
+        this.data = new Map();
+    }
+
+    public static getInstance() {
+        return this.instance != null ? this.instance : this.instance = new Live();
+    }
+}
+
+export { Live }
