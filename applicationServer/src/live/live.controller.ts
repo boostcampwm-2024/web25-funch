993cdd94e187193a44fced8f54a90e7329e40e25
JYKIM317 <augu202@naver.com>
2024-11-20 03:11:34 +0900

    refactor: 전체 방송 및 추천 방송 반환 API 리팩토링
    기존 방송 정보만 반환하던 로직에서 방송 정보와 스트리머의 정보를 함께
    전달할 수 있도록 Interceptor를 추가하였습니다.
    다만 이후 방송 정보에 스트리머 정보의 일부를 포함할지 논의가 필요할 것
    같습니다.

--- applicationServer/src/live/live.controller.ts
+++ applicationServer/src/live/live.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { LiveService } from '@live/live.service';
 import { SUGGEST_LIVE_COUNT } from '@src/constants';
+import { AddMemberInfoToLive } from '@src/utils/interceptor/live.interceptor';
 
 @Controller('live')
 export class LiveController {
   constructor(private readonly liveService: LiveService) {}
 
+  @AddMemberInfoToLive()
   @Get('/list')
   getLivelistAlignViewerCount(@Query('start') start: number, @Query('end') end: number) {
     return this.liveService.getLiveList(start, end);
@@ -16,8 +18,9 @@ export class LiveController {
     return this.liveService.responsePlaylistUrl(broadcastId);
   }
 
+  @AddMemberInfoToLive()
   @Get('/list/suggest')
   getSuggestLiveList() {
-    return { suggest: this.liveService.getCurrentLiveListRandomShuffle(SUGGEST_LIVE_COUNT) };
+    return this.liveService.getRandomLiveList(SUGGEST_LIVE_COUNT);
   }
 }
