4abe49e4162b5125ef5f53006a6a6150048d5f36
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-11-19 17:56:18 +0900

    refactor: 추천 방송 목록 수 상수화
    live.controller.ts에 사용되는 Suggest live count를 상수화 해놨습니다.

--- applicationServer/src/constants.ts
+++ applicationServer/src/constants.ts
@@ -1,3 +1,6 @@
 // member.providers.ts
 export const MEMBER_REPOSITORY = 'MEMBER_REPOSITORY';
 export const DATA_SOURCE = 'DATA_SOURCE';
+
+// live.controller.ts
+export const SUGGEST_LIVE_COUNT = 10;
