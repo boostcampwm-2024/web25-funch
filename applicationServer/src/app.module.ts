7300b7cb18f7349de67c0b19b502099133f1d21b
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 01:25:25 +0900

    feat: github 로그인 callback 구현
    - 사용자가 github 로그인을 통해 제공한 사용자 정보를 바탕으로 회원가입/로그인 로직 초반을 구현하였다.

--- applicationServer/src/app.module.ts
+++ applicationServer/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './member/member.module';
 import { LiveModule } from './live/live.module';
+import { GithubAuthModule } from '@auth/github/github.module';
 
 @Module({
-  imports: [MemberModule, LiveModule],
+  imports: [MemberModule, GithubAuthModule, LiveModule],
   controllers: [AppController],
   providers: [AppService],
 })
