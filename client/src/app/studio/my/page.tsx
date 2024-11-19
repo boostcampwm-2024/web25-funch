44eed34357e059be1d879c2266f6d95640f1a86b
Wille Lee <1992season@gmail.com>
2024-11-19 17:57:49 +0900

    feat: 내 방송 페이지 레이아웃 - 내 방송 페이지의 레이아웃을 구성해보았다.

--- client/src/app/studio/my/page.tsx
+++ client/src/app/studio/my/page.tsx
@@ -1,5 +1,16 @@
+import clsx from 'clsx';
+
 const StudioMyPage = () => {
-  return <div>page</div>;
+  return (
+    <div className={clsx('grid w-full grid-cols-[1fr,20rem] px-8')}>
+      <section>
+        <div className={clsx('pt-live-aspect-ratio relative w-full')}>
+          <div className={clsx('bg-surface-static-black absolute left-0 top-0 h-full w-full')} />
+        </div>
+      </section>
+      <section>채팅 영역</section>
+    </div>
+  );
 };
 
 export default StudioMyPage;
