ff5e22a3289992cb550d8de88aa7dc5d9772a21d
Wille Lee <1992season@gmail.com>
2024-11-20 02:07:16 +0900

    refactor: 테마 버튼, 스튜디오 버튼 aria-label 추가

--- client/src/app/components/layout/StudioBtn.tsx
+++ client/src/app/components/layout/StudioBtn.tsx
@@ -7,6 +7,7 @@ const StudioBtn = () => {
   const { push } = useInternalRouter();
   return (
     <HeaderControlButton
+      aria-label="펀치 스튜디오로 이동하기"
       componentType="STUDIO"
       onClick={() => {
         push('/studio');
