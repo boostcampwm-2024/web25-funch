674573bb38f3d3e86005309fe92ce3797c5efd69
최호빈 <hobinchoi0116@gmail.com>
2024-11-19 13:34:27 +0900

    refactor: databaseProvider 배열에 대한 리뷰 적용
    - DatabaseModule에 적용되는 providers가 databaseProvider 뿐이라 배열 형태가 아닌 단일 Provider로 개선하였다.

--- applicationServer/src/database/database.providers.ts
+++ applicationServer/src/database/database.providers.ts
@@ -5,23 +5,21 @@ import { DATA_SOURCE } from '@src/constants';
 
 dotenv.config();
 
-const databaseProviders = [
-  {
-    provide: DATA_SOURCE,
-    useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: [path.join(__dirname, '../**/*.entity.{ts,js}')],
-        synchronize: false,
-      });
-      return dataSource.initialize();
-    },
+const databaseProvider = {
+  provide: DATA_SOURCE,
+  useFactory: async () => {
+    const dataSource = new DataSource({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [path.join(__dirname, '../**/*.entity.{ts,js}')],
+      synchronize: false,
+    });
+    return dataSource.initialize();
   },
-];
+};
 
-export { databaseProviders };
+export { databaseProvider };
