21bfc7eec845f0470989f3a6178a09a3030b1e79
Wille Lee <1992season@gmail.com>
2024-11-19 13:55:04 +0900

    feat: StudioTextarea - 스튜디오 내 방송 페이지를 위한 Textarea 컴포넌트 #226

--- client/src/app/studio/page.tsx
+++ client/src/app/studio/page.tsx
@@ -1,9 +1,11 @@
 import StudioInput from '@components/studio/StudioInput';
+import { TextareaRendererForTest } from '@components/studio/StudioTextarea';
 
 const StudioPage = () => {
   return (
     <div className="w-80">
       <StudioInput />
+      <TextareaRendererForTest />
     </div>
   );
 };
