47bccafa62dd8bf54f40db2c7cd50a6329ecb4a2
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 01:23:38 +0900

    feat: member 조회 및 저장 서비스 구현
    - member를 아이디 및 이름으로 찾도록 구현하였다.
    - 전달받은 아이디를 통해 새로운 member를 저장하도록 구현하였다.

--- applicationServer/src/member/member.module.ts
+++ applicationServer/src/member/member.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@src/database/database.module';
-import { memberProviders } from '@member/member.providers';
+import { memberProvider } from '@member/member.providers';
 import { MemberController } from '@member/member.controller';
 import { MemberService } from '@member/member.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [MemberController],
-  providers: [...memberProviders, MemberService],
-  exports: [MemberService],
+  providers: [memberProvider, MemberService],
+  exports: [memberProvider, MemberService],
 })
 export class MemberModule {}
