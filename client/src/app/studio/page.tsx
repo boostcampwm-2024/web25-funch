4dc1461b5c67baccc0e50dd9a6b47eb206d0b0ea
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-19 15:16:02 +0900

    feat: StudioImageInput

--- client/src/app/studio/page.tsx
+++ client/src/app/studio/page.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import StudioImageInput from './StudioImageInput';
+import { useState } from 'react';
 import StudioInput from '@components/studio/StudioInput';
 import { TextareaRendererForTest } from '@components/studio/StudioTextarea';
 
 const StudioPage = () => {
+  const [image, setImage] = useState<File | null>(null);
   return (
     <div className="w-80">
       <StudioInput />
       <TextareaRendererForTest />
+      <StudioImageInput setImage={setImage} />
     </div>
   );
 };
