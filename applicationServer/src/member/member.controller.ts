ea6146809f2299edb18ae2ae24e45d16eccf786c
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 14:46:29 +0900

    chore: Auth 주요 로직을 core 폴더 내부로 이동
    - Auth 주요 로직을 담은 파일이 점점 많아져서 core 폴더 내부로 이동시켰다.

--- applicationServer/src/member/member.controller.ts
+++ applicationServer/src/member/member.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { MemberService } from './member.service';
+import { MemberService } from '@member/member.service';
 
 @Controller('members')
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
+  // TODO: 로그인 해야지 접근 가능
   @Get()
   async getMembers() {
     return this.memberService.findMembers();
