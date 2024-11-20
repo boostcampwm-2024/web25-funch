e099b67be28eb5c4c0c53e449b8cc18949b25268
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 20:25:25 +0900

    feat: CORS 설정
    - localhost와 https://www.funch.site/ 도메인에서의 요청만 허용하도록  CORS 설정을 활성화하였다.

--- applicationServer/src/main.ts
+++ applicationServer/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
-// TODO: cors 적용
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? 'localhost',
+    methods: '*',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 8080);
 }
+
 bootstrap();
