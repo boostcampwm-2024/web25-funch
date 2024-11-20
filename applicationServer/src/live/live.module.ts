2bbe39c9480527bdd52ee31cd33ba5f09a6823e1
JYKIM317 <augu202@naver.com>
2024-11-21 02:31:53 +0900

    refactor: live.moudle에 auth 관련 module 추가
    liveService에서 Auth 관련 로직을 수행하기 위해 관련 모듈을
    추가했습니다.

--- applicationServer/src/live/live.module.ts
+++ applicationServer/src/live/live.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { LiveService } from '@live/live.service';
 import { LiveController } from '@live/live.controller';
 import { MemberModule } from '@src/member/member.module';
+import { AuthModule } from '@src/auth/core/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MemberModule],
+  imports: [
+    MemberModule,
+    AuthModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [LiveController],
   providers: [LiveService],
 })
