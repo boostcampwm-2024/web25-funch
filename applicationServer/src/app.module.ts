7b22b320c096a8af6d13839fa3374347df68e39f
JYKIM317 <augu202@naver.com>
2024-11-21 00:48:41 +0900

    refactor: 미사용 모듈 제거
    미사용 app.service와 controller가 제거됨에 따라 app.module에 포함되어
    있던 코드를 제거하였습니다.

--- applicationServer/src/app.module.ts
+++ applicationServer/src/app.module.ts
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MemberModule } from '@member/member.module';
 import { LiveModule } from './live/live.module';
 import { GithubAuthModule } from '@github/github.module';
@@ -12,8 +10,6 @@ dotenv.config();
 
 @Module({
   imports: [MemberModule, GithubAuthModule, AuthModule, LiveModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
