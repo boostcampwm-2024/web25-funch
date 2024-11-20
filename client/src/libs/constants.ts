b16766b2705fc5c0d61edaf11b516c49b64d97f2
Wille Lee <1992season@gmail.com>
2024-11-21 01:18:18 +0900

    feat: fetcher 모듈, middleware, cookie

--- client/src/libs/constants.ts
+++ client/src/libs/constants.ts
@@ -6,7 +6,16 @@ export const APP_THEME = {
 export const LOCAL_STORAGE_THEME_KEY = 'theme';
 export const LOCAL_STORAGE_USER_KEY = 'funch-user';
 
+export const COOKIE_USER_KEY = 'funch-user';
+
 export const VIDEO_ICON_COMPONENT_TYPE = {
   DEFAULT: 'DEFAULT' as const,
   FULLSCREEN: 'FULLSCREEN' as const,
+};
+
+export const HTTP_METHOD = {
+  GET: 'GET' as const,
+  POST: 'POST' as const,
+  PUT: 'PUT' as const,
+  DELETE: 'DELETE' as const,
 };
