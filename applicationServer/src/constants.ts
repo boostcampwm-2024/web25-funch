652788af0d70a1c895fe7525748444c4e3fa4ed3
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 23:01:53 +0900

    chore: 사용하지 않는 상수 삭제
    - 깃 충돌을 해결하다가 지우지 못한 상수를 삭제하였다.

--- applicationServer/src/constants.ts
+++ applicationServer/src/constants.ts
@@ -5,8 +5,6 @@ export const DATA_SOURCE = 'DATA_SOURCE';
 export const SUGGEST_LIVE_COUNT = 10;
 // live.service.ts
 export const NOTIFY_LIVE_DATA_INTERVAL_TIME = 30000;
-// github.controller.ts
-export const GITHUB_LOGIN_ID = (id: string) => `Github@${id}`;
 // github.service.ts
 export const ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
 export const APPLICATION_JSON = 'application/json';
