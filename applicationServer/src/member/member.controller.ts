857729f3621a8e4125a567092b46d25326cbd857
JYKIM317 <augu202@naver.com>
2024-11-21 00:50:30 +0900

    feat: 개인 데이터 반환 API 구현
    스튜디오 페이지 등에서 사용될 본인의 정보 전체를 반환하는 API를
    작성하였습니다.

--- applicationServer/src/member/member.controller.ts
+++ applicationServer/src/member/member.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { MemberService } from '@member/member.service';
 import { NeedLoginGuard } from '@src/auth/core/auth.guard';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('members')
 @UseGuards(NeedLoginGuard)
 export class MemberController {
-  constructor(private readonly memberService: MemberService) {}
+  constructor(
+    private readonly memberService: MemberService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Get()
   async getMembers() {
     return this.memberService.findMembers();
+  }
+
+  @Get('/mydata')
+  getMyData(@Req() req: Request) {
+    const accessToken = req.headers['authorization']?.split(' ')[1];
+    const decodedPayload = this.jwtService.decode(accessToken);
+    return this.memberService.findOneMemberWithCondition({ id: decodedPayload.memberId });
   }
 }
