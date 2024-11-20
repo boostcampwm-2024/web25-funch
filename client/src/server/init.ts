3dc7aa0e6b47622dfc87ae05d62f7df2a7862937
Wille Lee <1992season@gmail.com>
2024-11-21 01:18:18 +0900

    feat: fetcher 모듈, middleware, cookie

--- client/src/server/init.ts
+++ client/src/server/init.ts
@@ -2,10 +2,10 @@ export const initMocks = async () => {
   if (process.env.NODE_ENV === 'production') return;
 
   if (typeof window === 'undefined') {
-    const { server } = await import('./node');
+    const { server } = await import('@server/node');
     server.listen({ onUnhandledRequest: 'bypass' });
   } else {
-    const { worker } = await import('./browser');
+    const { worker } = await import('@server/browser');
     await worker.start({ onUnhandledRequest: 'bypass' });
   }
 };
