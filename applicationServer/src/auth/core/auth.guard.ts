e6eff519e4c570c79d1b3af57e3c04f11e20c481
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 19:07:45 +0900

    feat: NeedRefreshTokenGuard 추가 및 /auth/refresh 요청에 적용
    - /auth/refresh 요청에는 만료된 accessToken이 요청에 들어올 수 있으므로 NeedRefreshTokenGuard를 새롭게 만들었다.

--- applicationServer/src/auth/core/auth.guard.ts
+++ applicationServer/src/auth/core/auth.guard.ts
@@ -1,15 +1,18 @@
 import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthService } from '@auth/auth.service';
+import { CookieService } from '../cookie/cookie.service';
+import { REFRESH_TOKEN } from '@src/constants';
 
 @Injectable()
 class NoNeedLoginGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const accessToken = request.headers['authorization']?.split(' ')[1];
-    const refreshToken = request.cookies?.['refreshToken'];
 
-    if (accessToken || refreshToken) {
-      throw new HttpException('이미 로그인이 되어있습니다.', HttpStatus.FORBIDDEN);
+    if (accessToken && this.authService.verifyToken(accessToken)) {
+      throw new HttpException('이미 로그인이 되어있습니다.', HttpStatus.BAD_REQUEST);
     }
     return true;
   }
@@ -22,24 +25,39 @@ class NeedLoginGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const accessToken = request.headers['authorization']?.split(' ')[1];
+
+    if (!accessToken) {
+      throw new HttpException('로그인이 필요합니다.', HttpStatus.UNAUTHORIZED);
+    }
+    this.authService.verifyToken(accessToken);
+    return true;
+  }
+}
+
+@Injectable()
+class NeedRefreshTokenGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly cookieService: CookieService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
     const refreshToken = request.cookies?.['refreshToken'];
 
-    if (!accessToken || !refreshToken) {
+    if (!refreshToken) {
       throw new HttpException('로그인이 필요합니다.', HttpStatus.UNAUTHORIZED);
     }
 
-    const accessTokenPayload = this.authService.verifyToken(accessToken);
-    const refreshTokenPayload = this.authService.verifyToken(refreshToken);
-    if (!accessTokenPayload || !refreshTokenPayload) {
-      throw new HttpException('Token이 유효하지 않습니다.', HttpStatus.FORBIDDEN);
-    }
-
-    const savedRefreshToken = this.authService.getRefreshToken(refreshTokenPayload.memberId);
+    const payload = this.authService.verifyToken(refreshToken);
+    const savedRefreshToken = this.authService.getRefreshToken(payload.memberId);
     if (savedRefreshToken !== refreshToken) {
+      this.cookieService.clearCookie(response, REFRESH_TOKEN);
       throw new HttpException('만료된 Refresh Token 입니다.', HttpStatus.UNAUTHORIZED);
     }
     return true;
   }
 }
 
-export { NoNeedLoginGuard, NeedLoginGuard };
+export { NoNeedLoginGuard, NeedLoginGuard, NeedRefreshTokenGuard };
