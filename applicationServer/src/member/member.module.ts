1ae6a297468c36dabf68547ff87b6a2e298cfe65
JYKIM317 <augu202@naver.com>
2024-11-21 00:50:30 +0900

    feat: 개인 데이터 반환 API 구현
    스튜디오 페이지 등에서 사용될 본인의 정보 전체를 반환하는 API를
    작성하였습니다.

--- applicationServer/src/member/member.module.ts
+++ applicationServer/src/member/member.module.ts
@@ -4,9 +4,16 @@ import { memberProvider } from '@member/member.providers';
 import { MemberController } from '@member/member.controller';
 import { MemberService } from '@member/member.service';
 import { AuthModule } from '@src/auth/core/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [MemberController],
   providers: [memberProvider, MemberService],
   exports: [memberProvider, MemberService],
