493bfd9567d61718bf4a4e1636ca301dcf258d1d
최호빈 <hobinchoi0116@gmail.com>
2024-11-20 23:00:26 +0900

    refactor: dotenv.config()를 한 번만 호출하도록 개선
    - dotenv.config()는 프로세스 전체에서 한 번만 호출되어도 환경 변수 사용이 가능하므로 수정하였다.

--- applicationServer/src/auth/github/github.controller.ts
+++ applicationServer/src/auth/github/github.controller.ts
@@ -1,4 +1,3 @@
-import dotenv from 'dotenv';
 import { Controller, Get, Query, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 import { MemberService } from '@src/member/member.service';
@@ -7,8 +6,6 @@ import { AuthService } from '@auth/auth.service';
 import { CookieService } from '@cookie/cookie.service';
 import { NoNeedLoginGuard } from '@auth/auth.guard';
 import { REFRESH_TOKEN } from '@src/constants';
-
-dotenv.config();
 
 @Controller('auth/github')
 class GithubAuthController {
