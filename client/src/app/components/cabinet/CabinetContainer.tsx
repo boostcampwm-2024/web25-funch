dbf9282e9e82dc69b7383d725034a98d964061dc
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-21 18:12:49 +0900

    feat: Cabinet
    - Link 적용
    - title (absolute -> fixed)

--- client/src/app/components/cabinet/CabinetContainer.tsx
+++ client/src/app/components/cabinet/CabinetContainer.tsx
@@ -2,25 +2,24 @@
 
 import { useState, useEffect } from 'react';
 import CabinetLink from './CabinetLink';
-import RefreshSvg from '@components/svgs/RefreshSvg';
-import DownArrowSvg from '@components/svgs/DownArrowSvg';
-import UpArrowSvg from '@components/svgs/UpArrowSvg';
 import FollowList from './FollowList';
 import useDesktop from '@hooks/useDesktop';
 import AccordionButton from '@components/AccordionButton';
+import SuggestedList from './SuggestedList';
+import DesktopHeader from './DesktopHeader';
 
 const CabinetContainer = () => {
   return (
-    <div className="funch-desktop:pt-16 flex h-full flex-col pt-20">
+    <div className="funch-desktop:pt-16 flex h-full w-full flex-col pt-20">
       <CategoryNavigator />
-      <StreamerNavigator />
+      <SuggestedNavigator />
     </div>
   );
 };
 
 const CategoryNavigator = () => {
   return (
-    <div className="funch-desktop:h-32 h-40 min-h-28">
+    <div className="funch-desktop:h-32 funch-desktop:min-h-32 h-40 min-h-40">
       <CabinetLink link="category" />
       <CabinetLink link="follow" />
     </div>
@@ -28,7 +27,7 @@ const CategoryNavigator = () => {
 };
 
 const StreamerNavigator = () => {
-  const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(false);
   const [isFolded, setIsFolded] = useState(false);
   const { isDesktop } = useDesktop();
 
@@ -42,9 +41,9 @@ const StreamerNavigator = () => {
     <>
       <div className="funch-desktop:w-5/6 border-y-surface-neutral-base mx-auto w-1/2 border-y-2 py-4 pb-2">
         {isDesktop ? (
-          <FollowDesktopHeader isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+          <DesktopHeader isExpanded={isExpanded} componentType="SUGGEST" setIsExpanded={setIsExpanded} />
         ) : (
-          <FollowNavHeader />
+          <NavHeader />
         )}
         <FollowList isDesktop={isDesktop} isFolded={isFolded} isExpanded={isExpanded} />
         {isExpanded && isDesktop && (
@@ -57,27 +56,40 @@ const StreamerNavigator = () => {
   );
 };
 
-type ExpandedProps = {
-  isExpanded: boolean;
-  setIsExpanded: (value: React.SetStateAction<boolean>) => void; // 타입도 더 정확하게 수정
-};
+const SuggestedNavigator = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isFolded, setIsFolded] = useState(false);
+  const { isDesktop } = useDesktop();
 
-const FollowNavHeader = () => {
+  useEffect(() => {
+    if (!isDesktop) {
+      setIsExpanded(true);
+    }
+  }, [isDesktop]);
+
   return (
-    <div className="text-content-neutral-strong w-full text-center">
-      <h2 className="funch-bold12">팔로우</h2>
-    </div>
+    <>
+      <div className="funch-desktop:w-5/6 border-y-surface-neutral-base mx-auto w-1/2 border-y-2 py-4 pb-2">
+        {isDesktop ? (
+          <DesktopHeader isExpanded={isExpanded} componentType="SUGGEST" setIsExpanded={setIsExpanded} />
+        ) : (
+          <NavHeader />
+        )}
+        <SuggestedList isDesktop={isDesktop} isFolded={isFolded} isExpanded={isExpanded} />
+        {isExpanded && isDesktop && (
+          <div className="flex justify-center">
+            <AccordionButton isExpanded={isFolded} toggle={() => setIsFolded((prev) => !prev)} />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
-const FollowDesktopHeader = ({ isExpanded, setIsExpanded }: ExpandedProps) => {
+const NavHeader = () => {
   return (
-    <div className="text-content-neutral-strong flex justify-between">
-      <h2 className="funch-bold14 funch-desktop:funch-bold16">팔로잉 채널</h2>
-      <div className="flex">
-        <RefreshSvg />
-        <button onClick={() => setIsExpanded((prev) => !prev)}>{isExpanded ? <UpArrowSvg /> : <DownArrowSvg />}</button>
-      </div>
+    <div className="text-content-neutral-strong w-full text-center">
+      <h2 className="funch-bold12">추천</h2>
     </div>
   );
 };
