97017efaf72faae1ce21c5f6fd04698176dd7888
JYKIM317 <augu202@naver.com>
2024-11-21 10:44:15 +0900

    refactor: 리뷰 반영 JWT.decode -> AuthService.verifyToken
    리뷰를 반영해 불필요한 JWT 모듈 호출을 제거하였고, AuthService의
    verifyToken 기능을 이용하도록 변경했습니다.

--- applicationServer/src/member/member.controller.ts
+++ applicationServer/src/member/member.controller.ts
@@ -2,14 +2,14 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { MemberService } from '@member/member.service';
 import { NeedLoginGuard } from '@src/auth/core/auth.guard';
 import { Request } from 'express';
-import { JwtService } from '@nestjs/jwt';
+import { AuthService } from '@src/auth/core/auth.service';
 
 @Controller('members')
 @UseGuards(NeedLoginGuard)
 export class MemberController {
   constructor(
     private readonly memberService: MemberService,
-    private readonly jwtService: JwtService,
+    private readonly authService: AuthService,
   ) {}
 
   @Get()
@@ -20,7 +20,7 @@ export class MemberController {
   @Get('/mydata')
   getMyData(@Req() req: Request) {
     const accessToken = req.headers['authorization']?.split(' ')[1];
-    const decodedPayload = this.jwtService.decode(accessToken);
+    const decodedPayload = this.authService.verifyToken(accessToken);
     return this.memberService.findOneMemberWithCondition({ id: decodedPayload.memberId });
   }
 }
