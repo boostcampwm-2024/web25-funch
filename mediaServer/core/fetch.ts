eeab32a7b4538e2a034628ffc23dbf3cb93136f0
JYKIM317 <augu202@naver.com>
2024-11-25 01:17:32 +0900

    refactor: mediaServer 스트리밍 이벤트 API 호출 함수 변경
    기존 중복된 코드였던 startStreaming과 endStreaming을 streamingEvent 함수
    하나로 변경하였고, 전달받는 인자값을 streamKey와 internalPath를 함께
    받을 수 있도록 객체 형태로 전달받도록 변경하였습니다.
    
    Co-authored-by: 최호빈 <hobinchoi0116@gmail.com>

--- mediaServer/core/fetch.ts
+++ mediaServer/core/fetch.ts
@@ -1,27 +1,21 @@
+import { logger } from '@/logger';
+
+type StreamingEvent = 'start' | 'end';
 const API_SERVER_LIVE_URL = 'https://api.funch.site/live';
 
-async function startStreaming(streamKey) {
-  return fetch(`${API_SERVER_LIVE_URL}/start`, {
-    method: 'POST',
-    cache: 'no-store',
-    headers: {
-      'Content-Type': 'application/json',
-      Connection: 'close',
-    },
-    body: JSON.stringify({ streamKey }),
-  });
+async function streamingEvent(event: StreamingEvent, data) {
+  try {
+    return fetch(`${API_SERVER_LIVE_URL}/${event}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (e) {
+    logger.error(`fetch start stream event: ${e}`);
+    return;
+  }
 }
 
-function endStreaming(streamKey) {
-  return fetch(`${API_SERVER_LIVE_URL}/end`, {
-    method: 'POST',
-    cache: 'no-store',
-    headers: {
-      'Content-Type': 'application/json',
-      Connection: 'close',
-    },
-    body: JSON.stringify({ streamKey }),
-  });
-}
-
-export { startStreaming, endStreaming };
+export { streamingEvent };
