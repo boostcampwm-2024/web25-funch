294f1afa4c6e3d9f3a9ae04d5dc5b7705751fcb4
JYKIM317 <augu202@naver.com>
2024-11-25 01:15:30 +0900

    fix: FFMpeg 480p 출력 제거
    CPU 사용량을 줄이기 위해 FFMpeg으로 m4v 파일을 만드는 과정에서 480p 생성
    코드를 제거하였습니다.
    
    Co-authored-by: 최호빈 <hobinchoi0116@gmail.com>

--- mediaServer/core/media/ffmpeg.ts
+++ mediaServer/core/media/ffmpeg.ts
@@ -15,10 +15,10 @@ const defaultOutputOptions = [
   '-hls_list_size 3',
   '-hls_segment_type fmp4',
   '-hls_flags split_by_time+independent_segments+omit_endlist',
-  '-preset veryfast',
+  '-preset ultrafast',
 ];
 
-function initializeFFMepg(ffmpegInputStream: PassThrough, storagePath: string) {
+function initializeFFMpeg(ffmpegInputStream: PassThrough, storagePath: string) {
   return ffmpeg()
     .input(ffmpegInputStream)
     .inputOptions(['-re'])
@@ -28,7 +28,7 @@ function initializeFFMepg(ffmpegInputStream: PassThrough, storagePath: string) {
     .videoCodec('libx264')
     .audioCodec('aac')
     .size('1920x1080')
-    .videoBitrate('5000k')
+    .videoBitrate('8000k')
     .audioBitrate('192k')
     .outputOptions([...defaultOutputOptions, '-hls_fmp4_init_filename chunkList_1080p_0_0.mp4'])
 
@@ -36,29 +36,19 @@ function initializeFFMepg(ffmpegInputStream: PassThrough, storagePath: string) {
     .videoCodec('libx264')
     .audioCodec('aac')
     .size('1280x720')
-    .videoBitrate('3000k')
+    .videoBitrate('5000k')
     .audioBitrate('128k')
     .outputOptions([...defaultOutputOptions, '-hls_fmp4_init_filename chunkList_720p_0_0.mp4'])
-
-    .output(`${storagePath}/chunklist_480p_.m3u8`)
-    .videoCodec('libx264')
-    .audioCodec('aac')
-    .size('854x480')
-    .videoBitrate('1500k')
-    .audioBitrate('96k')
-    .outputOptions([...defaultOutputOptions, '-hls_fmp4_init_filename chunkList_480p_0_0.mp4'])
 
     .output(`${storagePath}/dynamic_thumbnail.jpg`)
     .outputOptions(['-vf', 'fps=1/30', '-update', '1', '-s', '426x240'])
 
-    .on('end', (err, stdout, stderr) => {
-      logger.info(`Finished processing! err: ${err}, stdout: ${stdout}, stderr: ${stderr}`);
-    })
     .on('close', (code) => {
       logger.error(`FFmpeg process exited with code ${code}`);
     })
-    .on('error', (code) => {
-      logger.error(`FFmpeg process error with code ${code}`);
+    .on('error', (code, stdout, stderr) => {
+      logger.error(`FFmpeg process error with code: ${code}\nstdout: ${stdout}\nstderr: ${stderr}`);
+      initializeFFMpeg(ffmpegInputStream, storagePath);
     });
 }
 
@@ -67,16 +57,14 @@ function createMasterPlaylist(storagePath) {
 #EXTM3U
 #EXT-X-VERSION:7
 
-#EXT-X-STREAM-INF:BANDWIDTH=5711200,RESOLUTION=1920x1080,CODECS="avc1.64002a,mp4a.40.2"
+#EXT-X-STREAM-INF:BANDWIDTH=8711200,RESOLUTION=1920x1080,CODECS="avc1.64002a,mp4a.40.2"
 chunklist_1080p_.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=3440800,RESOLUTION=1280x720,CODECS="avc1.640020,mp4a.40.2"
+#EXT-X-STREAM-INF:BANDWIDTH=5440800,RESOLUTION=1280x720,CODECS="avc1.640020,mp4a.40.2"
 chunklist_720p_.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=1755600,RESOLUTION=854x480,CODECS="avc1.64001f,mp4a.40.2"
-chunklist_480p_.m3u8
 `.trim();
 
   const fileStream = fs.createWriteStream(`${storagePath}/master_playlist.m3u8`);
   fileStream.write(masterPlaylist, () => fileStream.end());
 }
 
-export { initializeFFMepg, createMasterPlaylist };
+export { initializeFFMpeg, createMasterPlaylist };
