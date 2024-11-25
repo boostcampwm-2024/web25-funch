e11b9371ee77e2a5e0a166b8f6a361b176644929
Wille Lee <1992season@gmail.com>
2024-11-26 00:11:51 +0900

    refactor: LiveInfo 각 요소 메모이제이션

--- client/src/app/(domain)/features/live/LiveInfo.tsx
+++ client/src/app/(domain)/features/live/LiveInfo.tsx
@@ -4,12 +4,17 @@ import HeartSvg from '@components/svgs/HeartSvg';
 import useLiveContext from '@hooks/useLiveContext';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
+import { memo, type PropsWithChildren, type ReactNode, useEffect, useRef, useState } from 'react';
 import Badge from '@app/(domain)/features/Badge';
 import { comma } from '@libs/formats';
+import type { Broadcast } from '@libs/internalTypes';
 
-const LiveInfo = () => {
-  const [isHovered, setIsHovered] = useState(false);
+type Props = {
+  children: (args: { liveInfo: Broadcast }) => ReactNode;
+};
+
+const LiveInfoWrapper = ({ children }: Props) => {
+  // const [isHovered, setIsHovered] = useState(false);
   const { liveInfo, refreshLiveInfo, broadcastId } = useLiveContext();
   const eventSourceRef = useRef<EventSource | null>(null);
 
@@ -46,7 +51,8 @@ const LiveInfo = () => {
 
   return (
     <div className="px-7 pb-6 pt-4">
-      <h3 className="funch-bold20 text-content-neutral-strong">{liveInfo.title}</h3>
+      {children({ liveInfo })}
+      {/* <h3 className="funch-bold20 text-content-neutral-strong">{liveInfo.title}</h3>
       <div className="mt-4 flex">
         <div
           className="relative h-16 w-16"
@@ -101,9 +107,98 @@ const LiveInfo = () => {
             <span className="funch-meta14">팔로우</span>
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
+
+const LiveInfoTitle = memo(({ title }: { title: string }) => {
+  return <h3 className="funch-bold20 text-content-neutral-strong">{title}</h3>;
+});
+
+const LiveInfoDetailWrapper = ({ children }: PropsWithChildren) => {
+  return <div className="mt-4 grid w-full grid-cols-[4rem,1fr,6rem] gap-2.5">{children}</div>;
+};
+
+const LiveInfoProfileImage = memo(({ profileImageUrl, userName }: { profileImageUrl: string; userName: string }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div className="relative h-16" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <div className="relative h-full w-full p-1.5">
+        <div className="relative h-full w-full overflow-hidden rounded-full">
+          <Image fill={true} sizes="100%" src={profileImageUrl} alt={`스트리머 ${userName}의 프로필 이미지`} />
+        </div>
+      </div>
+      <div
+        className={clsx(
+          'border-border-brand-base absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 rounded-full border-solid',
+          isHovered ? 'border-4' : 'border-2',
+        )}
+      />
+    </div>
+  );
+});
+
+const LiveInfoDescription = ({ children }: PropsWithChildren) => {
+  return <div className="w-60">{children}</div>;
+};
+
+const LiveInfoUserName = memo(({ userName }: { userName: string }) => {
+  return (
+    <h4
+      title={userName}
+      className="text-content-neutral-strong funch-bold16 w-full overflow-hidden text-ellipsis whitespace-nowrap"
+    >
+      {userName}
+    </h4>
+  );
+});
+
+const LiveInfoTags = memo(
+  ({ contentCategory, moodCategory, tags }: { contentCategory: string; moodCategory: string; tags: string[] }) => {
+    const allTags = [contentCategory, moodCategory, ...tags];
+
+    return (
+      <ul className="mt-1.5 flex flex-wrap gap-1">
+        {allTags.map((tag, idx) => (
+          <li key={idx}>
+            <Badge componentType="OUTLINE">{tag}</Badge>
+          </li>
+        ))}
+      </ul>
+    );
+  },
+);
+
+const LiveInfoViewerCount = memo(({ viewerCount }: { viewerCount: number }) => {
+  return <span className="funch-medium12 text-content-neutral-base">{comma(viewerCount)}명 시청 중</span>;
+});
+
+const LiveInfoFollowButton = () => {
+  return (
+    <div className="pt-5">
+      <button
+        className={clsx(
+          'inline-flex h-8 items-center gap-0.5 rounded-full pl-3.5 pr-4',
+          'bg-surface-brand-strong text-content-neutral-inverse hover:bg-surface-brand-base',
+        )}
+      >
+        <HeartSvg />
+        <span className="funch-meta14">팔로우</span>
+      </button>
+    </div>
+  );
+};
+
+const LiveInfo = Object.assign(LiveInfoWrapper, {
+  Title: LiveInfoTitle,
+  Wrapper: LiveInfoDetailWrapper,
+  ProfileImage: LiveInfoProfileImage,
+  Description: LiveInfoDescription,
+  UserName: LiveInfoUserName,
+  Tags: LiveInfoTags,
+  ViewerCount: LiveInfoViewerCount,
+  FollowButton: LiveInfoFollowButton,
+});
 
 export default LiveInfo;
