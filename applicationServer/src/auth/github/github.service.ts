3a57681de4289eeb419adbfad6aa90ae92f66de6
JYKIM317 <augu202@naver.com>
2024-11-26 02:02:10 +0900

    refactor: github 로그인 에러처리 추가
    github 로그인 service 로직에 axios시 2xx 응답이 돌아오지 않아 프로세스가
    종료되는 것을 방지하기 위해 catch로 에러를 처리할 수 있는 로직을
    추가했습니다. 추가로 getUserInfo의 authorization header의 token type을
    Token -> Bearer로 변경하였습니다.

--- applicationServer/src/auth/github/github.service.ts
+++ applicationServer/src/auth/github/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import axios from 'axios';
-import { ACCESS_TOKEN_URL, APPLICATION_JSON, RESOURCE_URL } from '@src/constants';
+import { GITHUB_ACCESS_TOKEN_URL, APPLICATION_JSON, GITHUB_RESOURCE_URL } from '@src/constants';
 
 @Injectable()
 class GithubAuthService {
@@ -9,14 +9,18 @@ class GithubAuthService {
   async getAccessToken(code: string): Promise<string> {
     const request = {
       code,
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRETS,
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
     };
-    const response = await axios.post(ACCESS_TOKEN_URL, request, {
-      headers: {
-        accept: APPLICATION_JSON,
-      },
-    });
+    const response = await axios
+      .post(GITHUB_ACCESS_TOKEN_URL, request, {
+        headers: {
+          accept: APPLICATION_JSON,
+        },
+      })
+      .catch(() => {
+        throw new HttpException('Github AccessToken 가져오기 실패', HttpStatus.UNAUTHORIZED);
+      });
 
     if (response.data.error) {
       throw new HttpException('Github AccessToken 가져오기 실패', HttpStatus.UNAUTHORIZED);
@@ -25,11 +29,15 @@ class GithubAuthService {
   }
 
   async getUserInfo(accessToken: string) {
-    const { data } = await axios.get(RESOURCE_URL, {
-      headers: {
-        Authorization: `token ${accessToken}`,
-      },
-    });
+    const { data } = await axios
+      .get(GITHUB_RESOURCE_URL, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .catch(() => {
+        throw new HttpException('Github 유저 정보 가져오기 실패', HttpStatus.FAILED_DEPENDENCY);
+      });
 
     return data;
   }
