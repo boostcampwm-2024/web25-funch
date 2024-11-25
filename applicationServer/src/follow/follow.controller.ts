c0fd7668201941661e1a0aa5da0e37b13aa5b408
최호빈 <hobinchoi0116@gmail.com>
2024-11-26 02:07:33 +0900

    feat: 사용자의 팔로워/팔로잉 목록 가져오는 API 구현
    - 사용자의 팔로워와 팔로잉 목록을 가져온다.

--- applicationServer/src/follow/follow.controller.ts
+++ applicationServer/src/follow/follow.controller.ts
@@ -39,4 +39,23 @@ export class FollowController {
       throw new HttpException('팔로우 중이지 않습니다.', HttpStatus.BAD_REQUEST);
     }
   }
+
+  @Get(':memberId')
+  async getFollows(@Param('memberId') memberId: string, @Query('search') search: string) {
+    const { condition, key } = this.getFollowConditions(search, memberId);
+
+    const results = await this.followService.findAllFollowWithCondition(condition);
+    const data = results.map((result) => result[key]);
+
+    return { [key]: data };
+  }
+
+  private getFollowConditions(search: string, memberId: string) {
+    if (search === FOLLOWERS) {
+      return { condition: { following: memberId }, key: FOLLOWERS };
+    } else if (search === FOLLOWING) {
+      return { condition: { follower: memberId }, key: FOLLOWING };
+    }
+    throw new HttpException('올바른 팔로워/팔로잉 정보 요청이 아닙니다.', HttpStatus.BAD_REQUEST);
+  }
 }
