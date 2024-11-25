3f73f2042ec089c740ee1d43f7f97cbff484a4a1
최호빈 <hobinchoi0116@gmail.com>
2024-11-25 23:48:13 +0900

    fix: DI 컨테이너에 DataSource를 주입할 때 문자열 토큰 방식이 아닌 클래스 참조를 사용하도록 변경
    - 기존의 DataSource 주입 방식은 문자열 토큰 방식이었다.
    이 방식은 여러 DataSource 즉, 여러 데이터베이스를 주입하려할 때 각 DataSource를 구분하는 용도로 주로 사용하므로
    하나의 데이터베이스만 사용하고 있는 우리 서비스에서는 DataSource라는 클래스를 참조하는 것이 더 적합하다고 생각하여 변경하였다.

--- applicationServer/src/constants.ts
+++ applicationServer/src/constants.ts
@@ -1,6 +1,5 @@
 // member.providers.ts
 export const MEMBER_REPOSITORY = 'MEMBER_REPOSITORY';
-export const DATA_SOURCE = 'DATA_SOURCE';
 // live.controller.ts
 export const SUGGEST_LIVE_COUNT = 10;
 // live.service.ts
--- applicationServer/src/member/member.providers.ts
+++ applicationServer/src/member/member.providers.ts
@@ -1,11 +1,11 @@
 import { DataSource } from 'typeorm';
 import { Member } from '@member/member.entity';
-import { MEMBER_REPOSITORY, DATA_SOURCE } from '@src/constants';
+import { MEMBER_REPOSITORY } from '@src/constants';
 
 const memberProvider = {
   provide: MEMBER_REPOSITORY,
   useFactory: (dataSource: DataSource) => dataSource.getRepository(Member),
-  inject: [DATA_SOURCE],
+  inject: [DataSource],
 };
 
 export { memberProvider };
