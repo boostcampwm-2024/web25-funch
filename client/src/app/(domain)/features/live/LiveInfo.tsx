96a8186d9462d10df8ea3fcdb4ca5effc55fe568
Wille Lee <1992season@gmail.com>
2024-11-26 00:18:52 +0900

    chore: production에서 콘솔 로그 미노출

--- client/src/app/(domain)/features/live/LiveInfo.tsx
+++ client/src/app/(domain)/features/live/LiveInfo.tsx
@@ -14,7 +14,6 @@ type Props = {
 };
 
 const LiveInfoWrapper = ({ children }: Props) => {
-  // const [isHovered, setIsHovered] = useState(false);
   const { liveInfo, refreshLiveInfo, broadcastId } = useLiveContext();
   const eventSourceRef = useRef<EventSource | null>(null);
 
@@ -49,67 +48,7 @@ const LiveInfoWrapper = ({ children }: Props) => {
     };
   }, [broadcastId]);
 
-  return (
-    <div className="px-7 pb-6 pt-4">
-      {children({ liveInfo })}
-      {/* <h3 className="funch-bold20 text-content-neutral-strong">{liveInfo.title}</h3>
-      <div className="mt-4 flex">
-        <div
-          className="relative h-16 w-16"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <div className="relative h-full w-full p-1.5">
-            <div className="relative h-full w-full overflow-hidden rounded-full">
-              <Image
-                fill={true}
-                sizes="100%"
-                src={liveInfo.profileImageUrl}
-                alt={`스트리머 ${liveInfo.userName}의 프로필 이미지`}
-              />
-            </div>
-          </div>
-          <div
-            className={clsx(
-              'border-border-brand-base absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 rounded-full border-solid',
-              isHovered ? 'border-4' : 'border-2',
-            )}
-          />
-        </div>
-        <div className="ml-2.5 w-60">
-          <h4
-            title={liveInfo.userName}
-            className="text-content-neutral-strong funch-bold16 w-full overflow-hidden text-ellipsis whitespace-nowrap"
-          >
-            {liveInfo.userName}
-          </h4>
-          {liveInfo.tags.length > 0 && (
-            <ul className="mt-1.5 flex gap-1">
-              {liveInfo.tags.map((tag, idx) => (
-                <li key={idx}>
-                  <Badge componentType="OUTLINE">{tag}</Badge>
-                </li>
-              ))}
-            </ul>
-          )}
-          <p className="mt-1">
-            <span className="funch-medium12 text-content-neutral-base">{comma(liveInfo.viewerCount)}명 시청 중</span>
-          </p>
-        </div>
-        <div className="ml-4 pt-5">
-          <button
-            className={clsx(
-              'inline-flex h-8 items-center gap-0.5 rounded-full pl-3.5 pr-4',
-              'bg-surface-brand-strong text-content-neutral-inverse hover:bg-surface-brand-base',
-            )}
-          >
-            <HeartSvg />
-            <span className="funch-meta14">팔로우</span>
-          </button>
-        </div>
-      </div> */}
-    </div>
-  );
+  return <div className="px-7 pb-6 pt-4">{children({ liveInfo })}</div>;
 };
 
 const LiveInfoTitle = memo(({ title }: { title: string }) => {
