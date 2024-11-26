197239324d479c5b83392d6d683015e6f40af236
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-26 22:00:33 +0900

    refactor: useFollowingLives 분리, provider globalProvider -> main 레이아웃으로 이동

--- client/src/app/GlobalProvider.tsx
+++ client/src/app/GlobalProvider.tsx
@@ -3,16 +3,13 @@
 import { type PropsWithChildren } from 'react';
 import UserProvider from '@providers/UserProvider';
 import ThemeProvider from '@providers/ThemeProvider';
-import { FollowingLivesProvider } from '@providers/FollowingLivesProvider';
 
 type Props = PropsWithChildren;
 
 const GlobalProvider = ({ children }: Props) => {
   return (
     <ThemeProvider>
-      <UserProvider>
-        <FollowingLivesProvider>{children}</FollowingLivesProvider>
-      </UserProvider>
+      <UserProvider>{children}</UserProvider>
     </ThemeProvider>
   );
 };
