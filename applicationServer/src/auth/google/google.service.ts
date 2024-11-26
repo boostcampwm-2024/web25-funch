64e87f261da456015fad3e712f1e9975c1dfb832
최호빈 <hobinchoi0116@gmail.com>
2024-11-26 10:22:23 +0900

    feat: 구글 로그인 모듈 세팅
    - 구글 로그인 구현을 위해 path alias를 설정하였다.
    - 구글 로그인 구현을 위해 Controller, Service를 생성하여 Module에 등록하였다.

---
+++ applicationServer/src/auth/google/google.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+class GoogleAuthService {
+  constructor() {}
+}
+
+export { GoogleAuthService };
