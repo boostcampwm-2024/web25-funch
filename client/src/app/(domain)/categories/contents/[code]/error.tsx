ad86a5ae55d1e9accd420349f65c5c3428c7218a
Wille Lee <1992season@gmail.com>
2024-11-26 16:14:16 +0900

    feat: 콘텐츠 카테고리 상세 페이지 - 콘텐츠 카테고리 상세 페이지(dynamic routing, static/dynamic rendering, server component) #267 - 라이브 그리드 컴포넌트를 재활용하여 목록 구현 #269 - 로딩(Suspense) 및 에러 바운더리(Next error.tsx)

---
+++ client/src/app/(domain)/categories/contents/[code]/error.tsx
@@ -0,0 +1,17 @@
+'use client'; // Error boundaries must be Client Components
+
+import Button from '@components/Button';
+import useInternalRouter from '@hooks/useInternalRouter';
+
+export default function Error({ reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  const { push } = useInternalRouter();
+  return (
+    <section className="min-h-home w-full">
+      <h2 className="mb-4 mt-10 text-center">라이브 목록을 불러오는데 실패했어요.</h2>
+      <div className="flex justify-center gap-2">
+        <Button onClick={() => reset()}>다시 시도하기</Button>
+        <Button onClick={() => push('/')}>홈으로</Button>
+      </div>
+    </section>
+  );
+}
--- client/src/app/(domain)/features/RecommendedLives.tsx
+++ client/src/app/(domain)/features/RecommendedLives.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import DeemedLink from '@components/DeemedLink';
+// import DeemedLink from '@components/DeemedLink';
 import clsx from 'clsx';
 import Lives from '@components/livesGrid/Lives';
 import { useEffect, useState } from 'react';
@@ -43,9 +43,9 @@ const RecommendedLives = () => {
 
   return (
     <div>
-      <div className={clsx('mb-2 flex items-center justify-between')}>
+      <div className={clsx('mb-4 flex items-center justify-between')}>
         <h2 className={clsx('text-content-neutral-primary funch-bold20')}>이 방송 어때요?</h2>
-        <DeemedLink url="/" name="전체보기" />
+        {/* <DeemedLink url="/" name="전체보기" /> */}
       </div>
       <Lives lives={lives}>
         {({ visibleLives, isExpanded, toggle }) => (
