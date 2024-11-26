2f59b4b9b49c07fab1c8dc8d0b5eb4e794ac6875
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-26 21:31:50 +0900

    refactor: 팔로우 Provider 생성
    - globalProvider에서 감싸도록 형성
    - FollowingLives, FollowingOffair에서 참조

--- client/src/app/GlobalProvider.tsx
+++ client/src/app/GlobalProvider.tsx
@@ -3,13 +3,16 @@
 import { type PropsWithChildren } from 'react';
 import UserProvider from '@providers/UserProvider';
 import ThemeProvider from '@providers/ThemeProvider';
+import { FollowingLivesProvider } from '@providers/FollowingLivesProvider';
 
 type Props = PropsWithChildren;
 
 const GlobalProvider = ({ children }: Props) => {
   return (
     <ThemeProvider>
-      <UserProvider>{children}</UserProvider>
+      <UserProvider>
+        <FollowingLivesProvider>{children}</FollowingLivesProvider>
+      </UserProvider>
     </ThemeProvider>
   );
 };
