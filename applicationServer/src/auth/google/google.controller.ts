58729cd147548889a98a75629fd75e4cad0583b8
최호빈 <hobinchoi0116@gmail.com>
2024-11-26 16:46:43 +0900

    feat: Google 로그인 API 구현
    - Google 로그인 요청을 통해 사용자의 자원을 가져와서 회원가입/로그인 로직을 처리한다.

--- applicationServer/src/auth/google/google.controller.ts
+++ applicationServer/src/auth/google/google.controller.ts
@@ -15,6 +15,22 @@ class GoogleAuthController {
     private readonly authService: AuthService,
     private readonly cookieService: CookieService,
   ) {}
+
+  @Post('/callback')
+  @UseGuards(NoNeedLoginGuard)
+  async getAccessToken(@Body('code') code: string, @Res({ passthrough: true }) res: Response) {
+    const googleAccessToken = await this.googleAuthService.getAccessToken(decodeURIComponent(code));
+    const { id, picture } = await this.googleAuthService.getUserInfo(googleAccessToken);
+
+    const member = await this.memberService.findOrRegisterMember(`Google@${id}`, picture);
+    const accessToken = this.authService.generateAccessToken(member.id);
+    const refreshToken = this.authService.generateRefreshToken(member.id);
+
+    this.authService.saveRefreshToken(member.id, refreshToken);
+    this.cookieService.setCookie(res, REFRESH_TOKEN, refreshToken);
+
+    return { accessToken, name: member.name, profile_image: member.profile_image, broadcast_id: member.broadcast_id };
+  }
 }
 
 export { GoogleAuthController };
