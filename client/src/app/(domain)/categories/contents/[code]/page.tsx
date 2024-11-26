222e48b40e9385904e5b9e81a72d68e316dffb4e
Wille Lee <1992season@gmail.com>
2024-11-26 16:14:16 +0900

    feat: 콘텐츠 카테고리 상세 페이지 - 콘텐츠 카테고리 상세 페이지(dynamic routing, static/dynamic rendering, server component) #267 - 라이브 그리드 컴포넌트를 재활용하여 목록 구현 #269 - 로딩(Suspense) 및 에러 바운더리(Next error.tsx)

--- client/src/app/(domain)/categories/contents/[code]/page.tsx
+++ client/src/app/(domain)/categories/contents/[code]/page.tsx
@@ -1,5 +1,64 @@
-const ContentCategoryPage = () => {
-  return <div>ContentCategoryPage</div>;
+import { Suspense } from 'react';
+import ContentCategory from './features/ContentCategory';
+import type { Broadcast, ContentsCategoryKey } from '@libs/internalTypes';
+import { getBroadcastsByContentCategory } from '@mocks/broadcasts';
+import clsx from 'clsx';
+import { CONTENTS_CATEGORY } from '@libs/constants';
+import { unstable_noStore as noStore } from 'next/cache';
+
+const fetchData = async (code: string): Promise<Broadcast[]> => {
+  if (process.env.NODE_ENV !== 'production') return getBroadcastsByContentCategory(code);
+
+  const response = await fetch(`/api/live/category?content=${code}`, {
+    cache: 'no-cache',
+  });
+
+  if (!response.ok) {
+    throw new Error('라이브 목록을 불러오는데 실패했어요.');
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+type Props = {
+  params: Promise<{
+    code: ContentsCategoryKey;
+  }>;
+};
+
+const ContentCategoryPage = async ({ params }: Props) => {
+  noStore();
+
+  const code = (await params).code;
+
+  const cateogryName = CONTENTS_CATEGORY[code].NAME;
+
+  return (
+    <section className="min-h-home w-full px-7">
+      <div className={clsx('mb-4 flex items-center')}>
+        <h2 className={clsx('text-content-neutral-primary funch-bold20')}>{cateogryName}</h2>
+      </div>
+      <Suspense fallback={<p>라이브 목록을 불러우는 중...</p>}>
+        <ContentCategoryFetcher code={code} />
+      </Suspense>
+    </section>
+  );
+};
+
+type FetcherProps = {
+  code: string;
+};
+
+const ContentCategoryFetcher = async ({ code }: FetcherProps) => {
+  const lives = await fetchData(code);
+
+  if (lives.length === 0) {
+    return <div>아직 방송 중인 스트리머가 없어요. 🥲</div>;
+  }
+
+  return <ContentCategory lives={lives} />;
 };
 
 export default ContentCategoryPage;
