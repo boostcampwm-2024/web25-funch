bfe255168bb458170df115f0611de3a17f0a01a8
JYKIM317 <augu202@naver.com>
2024-11-28 01:24:37 +0900

    test: llhls 시도
    llhls를 구현하기 위한 플레이리스트 생성 및 미디어 파일 .ts 변환 로직을
    구현했습니다. 미적용할 예정
    
    Co-authored-by: 최호빈 <hobinchoi0116@gmail.com>

--- mediaServer/core/hls/playlist.ts
+++ mediaServer/core/hls/playlist.ts
@@ -4,46 +4,63 @@ const EMPTY = '';
 const LF = '\n';
 const DOUBLE_LF = LF + LF;
 
-function writeMediaPlaylist(mediaSquenceNumber, quality, sequence, filePath) {
-  const header = generatePlaylistHeader(mediaSquenceNumber);
-  const newMediaSequence = generatePlaylistSegment(quality, sequence);
+function writeMediaPlaylist(mediaSquenceNumber, quality, sequence, filePath, startDate) {
+  const header = generatePlaylistHeader(mediaSquenceNumber, startDate);
   const newPreloadHint = generatePlaylistPreloadHint(quality, sequence);
+  let newMediaSequence: string = '';
 
   fs.readFile(filePath, 'utf-8', (err, buf) => {
     const data = buf == null ? [header, EMPTY].join(LF) : buf.toString();
     const beforeSegmentList = splitMediaSegment(data);
-    const beforeSegments = splitBeforeSegments(beforeSegmentList).trim();
+    const existed = findUncompletedSegment(data);
+    if (existed) {
+      newMediaSequence = generatePlaylistSegment(quality, sequence, startDate);
+    } else {
+      newMediaSequence = `#EXT-X-PART:DURATION=1.000000,URI="chunklist_${quality}p_part_${sequence}.ts",INDEPENDENT=YES`;
+    }
+    const beforeSegments = splitBeforeSegments(beforeSegmentList, existed).trim();
     const mediaPlaylist = [header, EMPTY, beforeSegments, newMediaSequence, newPreloadHint].join(LF);
-
     fs.writeFile(filePath, mediaPlaylist, () => {});
   });
 }
 
-function generatePlaylistHeader(mediaSquenceNumber) {
+function generatePlaylistHeader(mediaSquenceNumber, startDate) {
   return `#EXTM3U
 #EXT-X-VERSION:10
 #EXT-X-INDEPENDENT-SEGMENTS
-#EXT-X-TARGETDURATION:10
+#EXT-X-TARGETDURATION:2
 #EXT-X-SERVER-CONTROL:CAN-BLOCK-RELOAD=YES,PART-HOLD-BACK=3.002000
 #EXT-X-PART-INF:PART-TARGET=1.000000
 #EXT-X-DISCONTINUITY-SEQUENCE:0
 #EXT-X-MEDIA-SEQUENCE:${mediaSquenceNumber}
+#EXT-X-DATERANGE:ID="nmss-daterange",START-DATE="${startDate}"
 `.trim();
 }
 
-function generatePlaylistSegment(quality, sequence) {
+function generatePlaylistSegment(quality, sequence, startDate) {
+  const dateString = new Date(startDate);
+  dateString.setSeconds(dateString.getSeconds() + sequence);
   return `
-#EXT-X-PART:DURATION=1.000000,URI="chunklist_${quality}p_part_${sequence * 2}.ts",INDEPENDENT=YES
-#EXT-X-PART:DURATION=1.000000,URI="chunklist_${quality}p_part_${sequence * 2 + 1}.ts",INDEPENDENT=YES
+#EXT-X-PROGRAM-DATE-TIME:${dateString.toISOString()}
+#EXT-X-PART:DURATION=1.000000,URI="chunklist_${quality}p_part_${sequence - 1}.ts",INDEPENDENT=YES
+#EXT-X-PART:DURATION=1.000000,URI="chunklist_${quality}p_part_${sequence}.ts",INDEPENDENT=YES
 #EXTINF:2.000000,
-chunklist_${quality}p_${sequence}.ts
+chunklist_${quality}p_${Math.floor(sequence / 2)}.ts
 `.trim();
 }
 
 function generatePlaylistPreloadHint(quality, sequence) {
   return `
-#EXT-X-PRELOAD-HINT:TYPE=PART,URI="chunklist_${quality}p_part_${sequence * 2 + 2}.ts"
+#EXT-X-PRELOAD-HINT:TYPE=PART,URI="chunklist_${quality}p_part_${+sequence + 1}.ts"
 `.trim();
+}
+
+function findUncompletedSegment(data): boolean {
+  const [, segments] = data.split(DOUBLE_LF);
+  if (!segments) return false;
+  const splittedSegment: string[] = segments.split(LF);
+  const find = splittedSegment.slice(-2, -1).join('');
+  return /^#EXT-X-PART/.test(find);
 }
 
 function splitMediaSegment(data) {
@@ -66,11 +83,11 @@ function splitMediaSegment(data) {
   return segmentList;
 }
 
-function splitBeforeSegments(beforeSegmentList) {
-  if (beforeSegmentList.length < 3) {
-    return beforeSegmentList.join(LF);
+function splitBeforeSegments(beforeSegmentList, existed) {
+  if (beforeSegmentList.length === 3 && existed) {
+    return beforeSegmentList.slice(1).join(LF);
   }
-  return beforeSegmentList.slice(1).join(LF);
+  return beforeSegmentList.join(LF);
 }
 
 export { writeMediaPlaylist };
--- mediaServer/core/media/ffmpeg.ts
+++ mediaServer/core/media/ffmpeg.ts
@@ -10,7 +10,18 @@ import { writeMediaPlaylist } from '@hls/playlist';
 ffmpeg.setFfmpegPath(ffmpegPath.path);
 ffmpeg.setFfprobePath(ffprobePath.path);
 
+const defaultOutputOptions = [
+  '-map 0:v',
+  '-map 0:a',
+  '-preset ultrafast',
+  '-profile:v baseline',
+  '-level 4.1',
+  '-f segment',
+  '-g 30',
+];
+
 function initializeFFMpeg(ffmpegInputStream: PassThrough, storagePath: string) {
+  const startDate = new Date().toISOString();
   return ffmpeg()
     .input(ffmpegInputStream)
     .inputOptions(['-re'])
@@ -22,40 +33,36 @@ function initializeFFMpeg(ffmpegInputStream: PassThrough, storagePath: string) {
     .size('1920x1080')
     .videoBitrate('8000k')
     .audioBitrate('192k')
-    .outputOptions([
-      '-map 0:v',
-      '-map 0:a',
-      '-preset ultrafast',
-      '-f segment',
-      '-segment_time 2',
-      '-g 30',
-      '-reset_timestamps 1',
-    ])
+    .outputOptions([...defaultOutputOptions, '-segment_time 2'])
 
     .output(`${storagePath}/chunklist_1080p_part_%d.ts`)
-    .outputOptions([
-      '-map 0:v',
-      '-map 0:a',
-      '-preset ultrafast',
-      '-f segment',
-      '-segment_time 1',
-      '-g 30',
-      '-reset_timestamps 1',
-    ])
+    .videoCodec('libx264')
+    .audioCodec('aac')
+    .size('1920x1080')
+    .videoBitrate('8000k')
+    .audioBitrate('192k')
+    .outputOptions([...defaultOutputOptions, '-segment_time 1'])
 
     .output(`${storagePath}/dynamic_thumbnail.jpg`)
     .outputOptions(['-vf', 'fps=1/30', '-update', '1', '-s', '426x240'])
 
     .on('stderr', async (stderrLine) => {
       if (stderrLine.includes('Opening')) {
-        const match = stderrLine.match(/(chunklist_(.+)p_(\d+)\.ts)/);
+        const match = stderrLine.match(/(chunklist_(.+)p_part_(\d+)\.ts)/);
         if (match) {
-          const [, , quality, sequence] = match;
+          const [, fileName, quality, partSequence] = match;
+          const sequence = Math.ceil(partSequence / 2);
 
-          let mediaSquenceNumber = sequence - 2;
-          if (mediaSquenceNumber < 0) mediaSquenceNumber = 0;
-          const m3u8FilePath = path.join(__dirname, '../../', storagePath, `chunklist_${quality}p.m3u8`);
-          writeMediaPlaylist(mediaSquenceNumber, quality, sequence, m3u8FilePath);
+          const filePath = path.join(__dirname, '../../', storagePath, fileName);
+          fs.watchFile(filePath, (file) => {
+            if (file.size !== 0) {
+              fs.unwatchFile(filePath);
+              let mediaSquenceNumber = sequence - 2;
+              if (mediaSquenceNumber < 0) mediaSquenceNumber = 0;
+              const m3u8FilePath = path.join(__dirname, '../../', storagePath, `chunklist_${quality}p.m3u8`);
+              writeMediaPlaylist(mediaSquenceNumber, quality, partSequence, m3u8FilePath, startDate);
+            }
+          });
         }
       }
     })
@@ -76,9 +83,9 @@ function createMasterPlaylist(storagePath) {
 
 #EXT-X-STREAM-INF:BANDWIDTH=8711200,RESOLUTION=1920x1080,CODECS="avc1.64002a,mp4a.40.2"
 chunklist_1080p.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=5440800,RESOLUTION=1280x720,CODECS="avc1.640020,mp4a.40.2"
-chunklist_720p.m3u8
 `.trim();
+  /*#EXT-X-STREAM-INF:BANDWIDTH=5440800,RESOLUTION=1280x720,CODECS="avc1.640020,mp4a.40.2"
+  chunklist_720p.m3u8*/
 
   const fileStream = fs.createWriteStream(`${storagePath}/master_playlist.m3u8`);
   fileStream.write(masterPlaylist, () => fileStream.end());
