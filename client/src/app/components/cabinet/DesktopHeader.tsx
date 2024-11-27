a6d34cfc15ebc847e438e4594d32c6655858c71e
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-27 16:36:08 +0900

    feat: 리프레시 버튼 회전 이벤트 추가

--- client/src/app/components/cabinet/DesktopHeader.tsx
+++ client/src/app/components/cabinet/DesktopHeader.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import RefreshSvg from '@components/svgs/RefreshSvg';
 import DownArrowSvg from '@components/svgs/DownArrowSvg';
 import UpArrowSvg from '@components/svgs/UpArrowSvg';
+
+import clsx from 'clsx';
+import { useState } from 'react';
 
 const COMPONENT_TYPE = {
   FOLLOW: 'FOLLOW' as const,
@@ -16,13 +21,27 @@ type ExpandedProps = {
 };
 
 const DesktopHeader = ({ isExpanded, setIsExpanded, componentType }: ExpandedProps) => {
+  const [rotationCount, setRotationCount] = useState(0);
+
+  const handleClick = () => {
+    setRotationCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <div className="text-content-neutral-strong flex justify-between">
       <h2 className="funch-bold14 funch-desktop:funch-bold16">
         {componentType === COMPONENT_TYPE.SUGGEST ? '추천 채널' : '팔로우 채널'}
       </h2>
       <div className="flex">
-        <RefreshSvg />
+        <button
+          className={`transform ${rotationCount > 0 ? 'rotate-360' : ''} transition duration-1000 ease-in-out`}
+          style={{
+            transform: `rotate(${360 * rotationCount}deg)`,
+          }}
+          onClick={handleClick}
+        >
+          <RefreshSvg />
+        </button>
         <button onClick={() => setIsExpanded((prev) => !prev)}>{isExpanded ? <UpArrowSvg /> : <DownArrowSvg />}</button>
       </div>
     </div>
