a1d70115d6e06612cf340662c93923fb0dcfaab2
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-28 16:49:25 +0900

    fix: 에러 바운더리 수정
    - isError를 검사하기 전 isLoggedin 여부를 검사하도록 수정

--- client/src/app/(domain)/features/FollowingLives.tsx
+++ client/src/app/(domain)/features/FollowingLives.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import React, { ReactNode, type PropsWithChildren } from 'react';
 import useUser from '@hooks/useUser';
 import Lives from '@components/livesGrid/Lives';
 import clsx from 'clsx';
 import useFollowingLives from '@hooks/useFollowingLives';
 
-export const FollowingLives = () => {
+const FollowingLives = () => {
+  const { isLoggedin } = useUser();
   const { isError, isLoading, lives } = useFollowingLives();
+
+  if (!isLoggedin) return null;
 
   if (isError) {
     return <div>에러가 발생했습니다.</div>;
@@ -18,34 +20,24 @@ export const FollowingLives = () => {
   }
 
   return (
-    <FollowingLivesWrapper>
-      <div className={clsx('mb-4 w-full')}>
-        <div className={clsx('mb-2 flex items-center justify-between')}>
-          <h2 className={clsx('text-content-neutral-primary funch-bold20')}>팔로우 중인 방송</h2>
-        </div>
-        <Lives lives={lives}>
-          {({ visibleLives, isExpanded, toggle }) => (
-            <>
-              <Lives.List>
-                {visibleLives.map((live, index) => (
-                  <Lives.Live key={live.broadcastId} live={live} isPriority={index === 0} />
-                ))}
-              </Lives.List>
-              {lives.length > 3 && <Lives.Expand isExpanded={isExpanded} toggle={toggle} />}
-            </>
-          )}
-        </Lives>
+    <div className={clsx('mb-4 w-full')}>
+      <div className={clsx('mb-2 flex items-center justify-between')}>
+        <h2 className={clsx('text-content-neutral-primary funch-bold20')}>팔로우 중인 방송</h2>
       </div>
-    </FollowingLivesWrapper>
+      <Lives lives={lives}>
+        {({ visibleLives, isExpanded, toggle }) => (
+          <>
+            <Lives.List>
+              {visibleLives.map((live, index) => (
+                <Lives.Live key={live.broadcastId} live={live} isPriority={index === 0} />
+              ))}
+            </Lives.List>
+            {lives.length > 3 && <Lives.Expand isExpanded={isExpanded} toggle={toggle} />}
+          </>
+        )}
+      </Lives>
+    </div>
   );
 };
 
-const FollowingLivesWrapper = ({ children }: PropsWithChildren) => {
-  const { isLoggedin } = useUser();
-
-  if (!isLoggedin) {
-    return null;
-  }
-
-  return <>{children}</>;
-};
+export default FollowingLives;
