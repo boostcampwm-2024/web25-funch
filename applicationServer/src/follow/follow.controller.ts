5f9e535cdbff8071f5c4ebaf4cde243bd7a1d3b2
최호빈 <hobinchoi0116@gmail.com>
2024-11-28 21:04:12 +0900

    fix: 멤버의 방송 아이디에서 멤버 아이디를 추출하도록 변경

--- applicationServer/src/follow/follow.controller.ts
+++ applicationServer/src/follow/follow.controller.ts
@@ -14,27 +14,39 @@ import {
 import { NeedLoginGuard } from '@src/auth/core/auth.guard';
 import { FollowService } from '@follow/follow.service';
 import { FOLLOWER, FOLLOWERS, FOLLOWING } from '@src/constants';
+import { MemberService } from '@src/member/member.service';
 
 @Controller('follow')
 @UseGuards(NeedLoginGuard)
 export class FollowController {
-  constructor(private readonly followService: FollowService) {}
+  constructor(
+    private readonly followService: FollowService,
+    private readonly memberService: MemberService,
+  ) {}
 
   @Post()
   @HttpCode(201)
   async followMember(@Body('follower') follower: string, @Body('following') following: string) {
     if (follower === following) throw new HttpException('본인을 팔로우할 수 없습니다.', HttpStatus.BAD_REQUEST);
-    const isAlreadyFollowing = await this.followService.findOneFollowWithCondition({ follower, following });
+    const followerId = (await this.memberService.findOneMemberWithCondition({ broadcast_id: follower })).id;
+    const followingId = (await this.memberService.findOneMemberWithCondition({ broadcast_id: following })).id;
+    const isAlreadyFollowing = await this.followService.findOneFollowWithCondition({
+      follower: followerId,
+      following: followingId,
+    });
     if (isAlreadyFollowing) throw new HttpException('이미 팔로우 중입니다.', HttpStatus.BAD_REQUEST);
 
-    await this.followService.followMember(follower, following);
+    await this.followService.followMember(followerId, followingId);
   }
 
   @Delete()
   @HttpCode(204)
   async unfollowMember(@Body('follower') follower: string, @Body('following') following: string) {
     if (follower === following) throw new HttpException('본인을 언팔로우할 수 없습니다.', HttpStatus.BAD_REQUEST);
-    const result = await this.followService.unfollowMember(follower, following);
+    const followerId = (await this.memberService.findOneMemberWithCondition({ broadcast_id: follower })).id;
+    const followingId = (await this.memberService.findOneMemberWithCondition({ broadcast_id: following })).id;
+
+    const result = await this.followService.unfollowMember(followerId, followingId);
     if (result.affected === 0) {
       throw new HttpException('팔로우 중이지 않습니다.', HttpStatus.BAD_REQUEST);
     }
--- applicationServer/src/follow/follow.module.ts
+++ applicationServer/src/follow/follow.module.ts
@@ -4,9 +4,10 @@ import { followProvider } from '@follow/follow.providers';
 import { FollowController } from '@follow/follow.controller';
 import { FollowService } from '@follow/follow.service';
 import { AuthModule } from '@src/auth/core/auth.module';
+import { MemberModule } from '@src/member/member.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, MemberModule],
   controllers: [FollowController],
   providers: [followProvider, FollowService],
   exports: [FollowService],
