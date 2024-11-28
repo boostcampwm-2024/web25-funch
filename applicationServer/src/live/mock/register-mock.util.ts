c777ec6a1a58f9bd79c78079dcd81abfc62f76b4
JYKIM317 <augu202@naver.com>
2024-11-28 21:13:48 +0900

    refactor: 방송 mock데이터 고정 이름 할당
    제목이 곧 내용입니다.

--- applicationServer/src/live/mock/register-mock.util.ts
+++ applicationServer/src/live/mock/register-mock.util.ts
@@ -1,5 +1,3 @@
-import { generateRandomName } from '@src/auth/util/name';
-
 function registerMockLive(live) {
   const mockBroadcastIdList = [
     '872bc363-0d0c-4bde-a867-7b0edd076ad6',
@@ -8,21 +6,28 @@ function registerMockLive(live) {
     '58fefadc-3fbd-40ad-bce1-f769c02c887f',
     'fec06dd7-11b4-4efd-b69e-5a9dc93c3891',
   ];
+  const mockNameList = [
+    '부정적인회색늑대1145',
+    '완벽한고슴도치1272',
+    '열정적인눈표범1449',
+    '고상한닭1321',
+    '흐뭇한깃대말1177',
+  ];
+
   const contentCategoryList = ['music', 'talk', null, 'cook', 'mukbang'];
   const moodCategoryList = [null, 'calm', null, 'happy', null];
 
   mockBroadcastIdList.forEach((data, idx) => {
-    const name = generateRandomName();
     live.data.set(mockBroadcastIdList[idx], {
       broadcastId: mockBroadcastIdList[idx],
       broadcastPath: `${mockBroadcastIdList[idx]}`,
-      title: `${name}의 라이브 방송`,
+      title: `${mockNameList[idx]}의 라이브 방송`,
       contentCategory: contentCategoryList[idx],
       moodCategory: moodCategoryList[idx],
       tags: [`방송 ${idx}`],
       thumbnailUrl: `https://kr.object.ncloudstorage.com/media-storage/${mockBroadcastIdList[idx]}/dynamic_thumbnail.jpg`,
       viewerCount: 0,
-      userName: name,
+      userName: mockNameList[idx],
       profileImageUrl: 'https://kr.object.ncloudstorage.com/funch-storage/profile/profile_default.png',
     });
   });
