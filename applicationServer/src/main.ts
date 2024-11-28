cf622573ca0ea5cd69f40647d87d1ac63c4a2c59
최호빈 <hobinchoi0116@gmail.com>
2024-11-29 01:05:41 +0900

    feat: limit 옵션을 통해 파싱 허용치 증가
    - 파싱 허용치 최대를 5mb로 증가시켰다.

--- applicationServer/src/main.ts
+++ applicationServer/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LOCALHOST, CORS_ORIGIN, CORS_ORIGIN_WWW } from './constants';
+import { urlencoded, json } from 'body-parser';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ limit: '5mb', extended: true }));
 
   app.enableCors({
     origin: [LOCALHOST, CORS_ORIGIN, CORS_ORIGIN_WWW],
