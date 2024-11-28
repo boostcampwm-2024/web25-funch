abc124e0f6a8a16bffeaba72d1fe4733d45ea6f6
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-11-28 15:43:50 +0900

    fix: followingLivesWrapper에서 로그인 여부 식별

--- client/src/app/(domain)/features/FollowingLives.tsx
+++ client/src/app/(domain)/features/FollowingLives.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import React from 'react';
-import { Broadcast, User2 } from '@libs/internalTypes';
+import React, { ReactNode, type PropsWithChildren } from 'react';
 import useUser from '@hooks/useUser';
 import Lives from '@components/livesGrid/Lives';
 import clsx from 'clsx';
 import useFollowingLives from '@hooks/useFollowingLives';
 
 export const FollowingLives = () => {
-  const { isLoggedin } = useUser(); // 기존 useUser 훅
-
   const { isError, isLoading, lives } = useFollowingLives();
 
   if (isError) {
@@ -21,26 +18,34 @@ export const FollowingLives = () => {
   }
 
   return (
-    <>
-      {isLoggedin && (
-        <div className={clsx('mb-4 w-full')}>
-          <div className={clsx('mb-2 flex items-center justify-between')}>
-            <h2 className={clsx('text-content-neutral-primary funch-bold20')}>팔로우 중인 방송</h2>
-          </div>
-          <Lives lives={lives}>
-            {({ visibleLives, isExpanded, toggle }) => (
-              <>
-                <Lives.List>
-                  {visibleLives.map((live, index) => (
-                    <Lives.Live key={live.broadcastId} live={live} isPriority={index === 0} />
-                  ))}
-                </Lives.List>
-                {lives.length > 3 && <Lives.Expand isExpanded={isExpanded} toggle={toggle} />}
-              </>
-            )}
-          </Lives>
+    <FollowingLivesWrapper>
+      <div className={clsx('mb-4 w-full')}>
+        <div className={clsx('mb-2 flex items-center justify-between')}>
+          <h2 className={clsx('text-content-neutral-primary funch-bold20')}>팔로우 중인 방송</h2>
         </div>
-      )}
-    </>
+        <Lives lives={lives}>
+          {({ visibleLives, isExpanded, toggle }) => (
+            <>
+              <Lives.List>
+                {visibleLives.map((live, index) => (
+                  <Lives.Live key={live.broadcastId} live={live} isPriority={index === 0} />
+                ))}
+              </Lives.List>
+              {lives.length > 3 && <Lives.Expand isExpanded={isExpanded} toggle={toggle} />}
+            </>
+          )}
+        </Lives>
+      </div>
+    </FollowingLivesWrapper>
   );
+};
+
+const FollowingLivesWrapper = ({ children }: PropsWithChildren) => {
+  const { isLoggedin } = useUser();
+
+  if (!isLoggedin) {
+    return null;
+  }
+
+  return <>{children}</>;
 };
