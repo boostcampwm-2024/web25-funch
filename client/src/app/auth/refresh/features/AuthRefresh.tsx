0bd4a55d8d4f3948ba935847ce170ba1af70e3f0
Wille Lee <1992season@gmail.com>
2024-12-01 20:25:24 +0900

    feat: AuthRefresh - 엑세스토큰 재발급 액션 한 번만 호출하도록 변수 설정 #317 - unauthorized status code를 constants에 저장

--- client/src/app/auth/refresh/features/AuthRefresh.tsx
+++ client/src/app/auth/refresh/features/AuthRefresh.tsx
@@ -4,21 +4,31 @@ import useUser from '@hooks/useUser';
 import { refreshAccessToken } from '@libs/actions';
 import { COOKIE_USER_KEY } from '@libs/constants';
 import Cookies from 'js-cookie';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const AuthRefresh = () => {
+  const isValidCallRef = useRef(true);
   const { logout } = useUser();
   useEffect(() => {
+    let isValidEffect = true;
     (async () => {
       try {
+        if (!isValidCallRef.current) return;
         const { accessToken } = await refreshAccessToken();
+        if (!isValidEffect) return;
+        isValidCallRef.current = false;
         Cookies.set(COOKIE_USER_KEY, accessToken);
         location.href = '/';
       } catch (err) {
+        if (!isValidEffect) return;
         alert('다시 로그인해주세요.');
         logout();
       }
     })();
+
+    return () => {
+      isValidEffect = false;
+    };
   }, []);
   return null;
 };
