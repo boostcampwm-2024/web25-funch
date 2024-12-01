a951b88803f6f9c93dfcedfa8828336be2d1cecf
JYKIM317 <augu202@naver.com>
2024-12-02 04:22:42 +0900

    feat: Application Server Search API 구현
    검색에 대한 정보 제공을 위한 Application Server의 API를 추가하였습니다.
    사용방법: [GET] api.funch.site/search?query=검색어

--- applicationServer/src/app.module.ts
+++ applicationServer/src/app.module.ts
@@ -6,13 +6,23 @@ import { LiveModule } from '@live/live.module';
 import { GithubAuthModule } from '@github/github.module';
 import { AuthModule } from '@auth/auth.module';
 import { FollowModule } from '@follow/follow.module';
+import { SearchModule } from './search/search.module';
 import { NaverAuthModule } from '@naver/naver.module';
 import { GoogleAuthModule } from '@google/google.module';
 
 dotenv.config();
 
 @Module({
-  imports: [MemberModule, GithubAuthModule, AuthModule, LiveModule, FollowModule, NaverAuthModule, GoogleAuthModule],
+  imports: [
+    MemberModule,
+    GithubAuthModule,
+    AuthModule,
+    LiveModule,
+    SearchModule,
+    FollowModule,
+    NaverAuthModule,
+    GoogleAuthModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ applicationServer/src/search/search.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { MemberService } from '@src/member/member.service';
+import { LiveService } from '@src/live/live.service';
+import { Like } from 'typeorm';
+import { User } from '@src/types';
+
+@Injectable()
+export class SearchService {
+  constructor(
+    private readonly memberService: MemberService,
+    private readonly liveService: LiveService,
+  ) {}
+
+  getLiveListWithKeyword(keyword) {
+    const currentLiveList = this.liveService.getLiveList(0);
+    const filteredLiveList = currentLiveList.reduce((list, live) => {
+      const metadata = [...live.tags, live.contentCategory, live.moodCategory, live.title, live.userName]
+        .join('')
+        .replace(' ', '');
+      if (metadata.match(new RegExp(keyword))) list.push(live);
+      return list;
+    }, []);
+
+    return filteredLiveList;
+  }
+
+  async getMemberListWithKeyword(keyword) {
+    const condition = { name: Like(`%${keyword}%`) };
+    const searchMemberList = await this.memberService.findMembersWithCondition(condition);
+
+    return searchMemberList.map((member) => {
+      return {
+        name: member.name,
+        profile_image: member.profile_image,
+        broadcast_id: member.broadcast_id,
+        follower_count: member.follower_count,
+      } as User;
+    });
+  }
+}
