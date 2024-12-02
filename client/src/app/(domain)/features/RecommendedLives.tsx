63a78d1bc4f351b1acd2a25f20b656ae21877aa4
Wille Lee <1992season@gmail.com>
2024-12-03 00:38:03 +0900

    refactor: /live/list staletime 지정 및 렌더링 구조 개선

--- client/src/app/(domain)/features/RecommendedLives.tsx
+++ client/src/app/(domain)/features/RecommendedLives.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import clsx from 'clsx';
 import RecommendedLivesRenderer from './RecommendedLivesRenderer';
-import { Suspense } from 'react';
 import ErrorBoundary from '@components/ErrorBoundary';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getLiveList } from '@libs/actions';
@@ -10,18 +8,9 @@ import { TANSTACK_QUERY_KEY } from '@libs/constants';
 
 const RecommendedLives = () => {
   return (
-    <div>
-      <div className={clsx('mb-4 flex items-center justify-between')}>
-        <h2 className={clsx('text-content-neutral-primary funch-bold20')}>이 방송 어때요?</h2>
-      </div>
-      <ErrorBoundary
-        fallback={<p className="funch-bold16 text-content-neutral-strong">추천 목록을 불러올 수 없어요.</p>}
-      >
-        <Suspense fallback={<p className="funch-bold16 text-content-neutral-strong">추천 목록을 불러오고 있어요.</p>}>
-          <RecommendedLivesFetcher />
-        </Suspense>
-      </ErrorBoundary>
-    </div>
+    <ErrorBoundary fallback={<p className="funch-bold16 text-content-neutral-strong">추천 목록을 불러올 수 없어요.</p>}>
+      <RecommendedLivesFetcher />
+    </ErrorBoundary>
   );
 };
 
@@ -29,7 +18,7 @@ const RecommendedLivesFetcher = () => {
   const { data: lives } = useSuspenseQuery({
     queryKey: [TANSTACK_QUERY_KEY.LIVE_LIST],
     queryFn: async () => await getLiveList(),
-    refetchOnMount: true,
+    staleTime: 1000 * 60,
   });
 
   return <RecommendedLivesRenderer lives={lives} />;
--- client/src/app/(domain)/page.tsx
+++ client/src/app/(domain)/page.tsx
@@ -1,11 +1,20 @@
 import RecommendedLives from './features/RecommendedLives';
 import FollowingLives from './features/FollowingLives';
+import clsx from 'clsx';
+import { Suspense } from 'react';
 
 const HomePage = () => {
   return (
     <section className="min-h-home w-full px-7">
       <FollowingLives />
-      <RecommendedLives />
+      <div>
+        <div className={clsx('mb-4 flex items-center justify-between')}>
+          <h2 className={clsx('text-content-neutral-primary funch-bold20')}>이 방송 어때요?</h2>
+        </div>
+        <Suspense fallback={<p className="funch-bold16 text-content-neutral-strong">추천 목록을 불러오고 있어요.</p>}>
+          <RecommendedLives />
+        </Suspense>
+      </div>
     </section>
   );
 };
