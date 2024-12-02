39ca6fcd9cb13a79404ce1a2a895f65dd0bce3d8
Wille Lee <1992season@gmail.com>
2024-12-02 17:21:10 +0900

    fix: searchbar

--- client/src/app/components/layout/Search.tsx
+++ client/src/app/components/layout/Search.tsx
@@ -2,17 +2,32 @@
 
 import { type ChangeEvent, useState } from 'react';
 import SearchView from './SearchView';
+import useInternalRouter from '@hooks/useInternalRouter';
 
 const Search = () => {
+  const { push } = useInternalRouter();
   const [input, setInput] = useState('');
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => setInput(e.target.value);
   const resetInput = () => setInput('');
-  const handleSubmit = () => {
+  const handleSubmit = (searchInput: string) => {
+    if (!searchInput) return;
     resetInput();
-    alert('안녕하세요, FUNCH입니다. ^o^\n검색 기능은 준비 중이에요.');
+    // alert('안녕하세요, FUNCH입니다. ^o^\n검색 기능은 준비 중이에요.');
+    push(`/search?query=${searchInput}`);
   };
   return (
-    <SearchView onSubmit={handleSubmit}>
+    <SearchView
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit(input);
+      }}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          handleSubmit(input);
+        }
+      }}
+    >
       <SearchView.Input value={input} onChange={handleChangeInput} />
       {input.length > 0 && <SearchView.Cancel onClick={resetInput} />}
       <SearchView.Button />
