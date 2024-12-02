3209084dc12adf27d5f326178355a894486c134c
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-12-02 18:13:15 +0900

    feat: search 페이지 라우트
    - params 받아 콘솔에 찍어봤음

---
+++ client/src/app/(domain)/search/page.tsx
@@ -0,0 +1,11 @@
+import SearchContainer from './feature/SearchContainer';
+
+const SearchPage = () => {
+  return (
+    <section className="min-h-home w-full px-7">
+      <SearchContainer />
+    </section>
+  );
+};
+
+export default SearchPage;
