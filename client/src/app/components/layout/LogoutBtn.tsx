efdbbd2dbddf746e1c362bc6d141c8d4d57cf97c
Wille Lee <1992season@gmail.com>
2024-12-03 02:36:52 +0900

    fix: 로그아웃 로직에서 컨펌 알러트 제거 및 로그아웃 버튼에서 실행 <- 재인증 중 로그아웃에서 불필요 알러트 발생 관련

--- client/src/app/components/layout/LogoutBtn.tsx
+++ client/src/app/components/layout/LogoutBtn.tsx
@@ -5,7 +5,18 @@ import useUser from '@hooks/useUser';
 
 const LogoutBtn = () => {
   const { logout } = useUser();
-  return <Button onClick={logout}>로그아웃</Button>;
+  return (
+    <Button
+      onClick={() => {
+        const ok = confirm('로그아웃하시겠어요?');
+        if (ok) {
+          logout();
+        }
+      }}
+    >
+      로그아웃
+    </Button>
+  );
 };
 
 export default LogoutBtn;
