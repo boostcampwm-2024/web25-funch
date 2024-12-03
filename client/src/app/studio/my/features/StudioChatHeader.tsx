651d8b4a9914d70c912af76594033313749fbb01
Wille Lee <1992season@gmail.com>
2024-12-03 20:49:35 +0900

    refactor: studio chat demo를 studio chat으로

--- client/src/app/studio/my/features/StudioChatHeader.tsx
+++
@@ -1,9 +0,0 @@
-const StudioChatHeader = () => {
-  return (
-    <header className="bg-surface-neutral-weak funch-bold16 border-border-neutral-weak flex h-10 items-center border-b px-4">
-      채팅
-    </header>
-  );
-};
-
-export default StudioChatHeader;
--- client/src/app/studio/my/page.tsx
+++ client/src/app/studio/my/page.tsx
@@ -1,22 +1,18 @@
 import { type Metadata } from 'next';
 import clsx from 'clsx';
-import MyStudioChat from './features/MyStudioChat';
 import MyStudioController from './features/MyStudioController';
+import MyStudioChatRenderer from './features/MyStudioChatRenderer';
 
 export const metadata: Metadata = {
-  title: '스튜디오',
+  title: '나의 스튜디오',
+  description: '나의 펀치 스튜디오에서 시청자와 소통하세요!',
 };
 
 const StudioMyPage = () => {
   return (
-    <div
-      className={clsx(
-        'h-live-section mx-auto grid w-full max-w-[calc(48rem+20rem+4rem)] grid-cols-[1fr,20rem]',
-        //'border-border-neutral-weak border-x',
-      )}
-    >
+    <div className={clsx('h-live-section mx-auto grid w-full max-w-[calc(48rem+20rem+4rem)] grid-cols-[1fr,20rem]')}>
       <MyStudioController />
-      <MyStudioChat />
+      <MyStudioChatRenderer />
     </div>
   );
 };
