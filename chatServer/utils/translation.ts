dee52adb3cced38887e2693f1ad276c9193bf8cd
JYKIM317 <augu202@naver.com>
2024-12-04 01:55:44 +0900

    feat: 채팅 서버 번역 기능 추가 #329
    Clova Studio API를 활용해 필요하다면 번역을 수행하는 기능을 채팅 서버에 포함했습니다.
    
    Co-authored-by: 최호빈 <hobinchoi0116@gmail.com>

---
+++ chatServer/utils/translation.ts
@@ -0,0 +1,71 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const CLOVA_ENDPOINT = 'https://clovastudio.stream.ntruss.com/testapp/v1/chat-completions/HCX-003';
+
+const translationMap = new Map([
+  ['korean', '한국어'],
+  ['english', '영어'],
+  ['japanese', '일본어'],
+  ['chinese', '중국어'],
+]);
+
+function fetchTranslate(text) {
+  const prompt = {
+    messages: [
+      {
+        role: 'system',
+        content: '- 주어진 주제를 한국어, 영어, 일본어, 중국어로 생성합니다.\n',
+      },
+      {
+        role: 'user',
+        content: text,
+      },
+    ],
+    topP: 0.8,
+    topK: 0,
+    maxTokens: 512,
+    temperature: 0.3,
+    repeatPenalty: 3.0,
+    stopBefore: [],
+    includeAiFilters: true,
+    seed: 0,
+  };
+
+  return fetch(CLOVA_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'text/event-stream',
+      'X-NCP-CLOVASTUDIO-API-KEY': process.env.X_NCP_CLOVASTUDIO_API_KEY,
+      'X-NCP-APIGW-API-KEY': process.env.X_NCP_APIGW_API_KEY,
+      'X-NCP-CLOVASTUDIO-REQUEST-ID': process.env.X_NCP_CLOVASTUDIO_REQUEST_ID,
+    },
+    body: JSON.stringify(prompt),
+  });
+}
+
+async function translate(text, lang?) {
+  if (!lang || lang === 'null') return text;
+
+  const response = await (await fetchTranslate(text)).text();
+  const results = response.trim().split('\n\n');
+
+  for (let idx = results.length - 1; idx >= 0; idx--) {
+    if (results[idx].match(/event:result/)) {
+      const result = results[idx].match(/data:(.+)/);
+      const data = JSON.parse(result[1]);
+      const translates = data.message.content.split('\n').reduce((obj, cur) => {
+        const [key, value] = cur.split(':');
+        obj[key.trim()] = value.trim();
+        return obj;
+      }, {});
+
+      return translates[translationMap.get(lang)];
+    }
+  }
+
+  return text;
+}
+
+export { translate };
