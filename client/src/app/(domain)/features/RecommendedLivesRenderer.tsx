e71750f9c611994e32e837cc16cbc91411841d6e
Wille Lee <1992season@gmail.com>
2024-12-05 00:15:27 +0900

    hotfix: 홈 라이브 목록 length=0 UI

--- client/src/app/(domain)/features/RecommendedLivesRenderer.tsx
+++ client/src/app/(domain)/features/RecommendedLivesRenderer.tsx
@@ -3,11 +3,23 @@
 import Lives from '@components/livesGrid/Lives';
 import type { Broadcast } from '@libs/internalTypes';
 import useFollowingLives from '@hooks/useFollowingLives';
+import Image from 'next/image';
+import noContents from '@assets/no_result.png';
+import clsx from 'clsx';
 
 const RecommendedLivesRenderer = ({ lives }: { lives: Broadcast[] }) => {
   const { ids } = useFollowingLives();
 
   const filteredLives = lives?.filter((live) => !ids.includes(live.broadcastId)) || [];
+
+  if (filteredLives.length === 0) {
+    return (
+      <div className={clsx('flex flex-col items-center')}>
+        <Image src={noContents} alt="no-following-lives" width={200} height={200} />
+        <p className={clsx('text-content-neutral-primary funch-medium16 mt-4')}>라이브 중인 방송이 없습니다.</p>
+      </div>
+    );
+  }
 
   return (
     <Lives lives={filteredLives}>
