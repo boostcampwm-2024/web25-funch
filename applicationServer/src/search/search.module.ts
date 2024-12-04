a26124df046411d79546f78f26d4d541988815da
JYKIM317 <augu202@naver.com>
2024-12-04 22:39:14 +0900

    feat: 방송 정보 저장소 마이그레이션 #356
    방송 데이터의 저장 위치를 인메모리에서 레디스로 마이그레이션했습니다.

--- applicationServer/src/search/search.module.ts
+++ applicationServer/src/search/search.module.ts
@@ -3,10 +3,11 @@ import { LiveModule } from '@live/live.module';
 import { MemberModule } from '@src/member/member.module';
 import { SearchController } from '@search/search.controller';
 import { SearchService } from '@search/search.service';
+import { RedisService } from '@database/redis.service';
 
 @Module({
   imports: [MemberModule, LiveModule],
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, RedisService],
 })
 export class SearchModule {}
