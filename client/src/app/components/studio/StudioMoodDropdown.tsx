f297b475d483f397934fd6e7a567d453c9f2d541
Wille Lee <1992season@gmail.com>
2024-12-04 15:42:20 +0900

    refactor: MyStudioForm 리팩토링 - aria-label 수정 - 상태 통합 테스트

--- client/src/app/components/studio/StudioMoodDropdown.tsx
+++ client/src/app/components/studio/StudioMoodDropdown.tsx
@@ -1,30 +1,32 @@
 import StudioDropdown from './StudioDropdown';
-import { useState, useEffect } from 'react';
-import { MoodsCategoryKey } from '@libs/internalTypes';
+// import { useState, useEffect } from 'react';
+// import { MoodsCategoryKey } from '@libs/internalTypes';
 import { MOODS_CATEGORY } from '@libs/constants';
 import MoodsCategoryPalette from '@app/(domain)/categories/features/MoodsCategoryPalette';
 
 type MoodProps = {
-  setData: (data: string) => void;
+  setData: (data: string) => void; // <- 무드 카테고리 키 세터?
   placeHolder: string;
-  data: string;
+  data: string; // <- 무드 카테고리 키? 코드?
 };
 
 const StudioMoodDropdown = ({ setData, data, placeHolder }: MoodProps) => {
   const moods = Object.values(MOODS_CATEGORY);
-  const [selectedKey, setSelectedKey] = useState<string | null>('');
-  const [selectedMoodCode, setSelectedMoodCode] = useState<MoodsCategoryKey>('unknown');
+  // const [selectedKey, setSelectedKey] = useState<string | null>(''); // <-
+  // const [selectedMoodCode, setSelectedMoodCode] = useState<MoodsCategoryKey>('unknown');
 
-  const selectMood = (key: string) => {
-    setSelectedKey(key);
-    setData(key);
-  };
+  // const selectMood = (key: string) => {
+  //   setSelectedKey(key);
+  //   setData(key);
+  // };
 
-  useEffect(() => {
-    console.log(data);
-    setSelectedKey(data);
-    setSelectedMoodCode(moods.find((m) => m.CODE === data)?.CODE || 'unknown');
-  }, [data]);
+  const selectedMood = moods.find((m) => m.CODE === data);
+
+  // useEffect(() => {
+  //   console.log(data);
+  //   setSelectedKey(data);
+  //   setSelectedMoodCode(moods.find((m) => m.CODE === data)?.CODE || 'unknown');
+  // }, [data]);
 
   return (
     <StudioDropdown>
@@ -45,9 +47,10 @@ const StudioMoodDropdown = ({ setData, data, placeHolder }: MoodProps) => {
                   <StudioDropdown.Item
                     key={idx}
                     onClick={() => {
-                      selectMood(m.CODE);
+                      // selectMood(m.CODE);
+                      setData(m.CODE);
                       blurDropdown();
-                      setSelectedMoodCode(m.CODE);
+                      // setSelectedMoodCode(m.CODE);
                     }}
                   >
                     <span>{m.NAME}</span>
@@ -55,11 +58,17 @@ const StudioMoodDropdown = ({ setData, data, placeHolder }: MoodProps) => {
                 ))}
             </StudioDropdown.List>
           )}
-          {selectedKey && (
-            <div className="relative mt-2 h-10 w-full shadow-md">
-              {MoodsCategoryPalette({ code: selectedMoodCode })}
-              <div className="text-content-neutral-inverse absolute left-[45%] top-[20%]">
-                {moods.find((m) => m.CODE === selectedKey)?.NAME}
+          {data && (
+            <div
+              aria-label={`무드 카테도리로 '${selectedMood?.NAME}'이 선택되어 있어요.`}
+              className="relative mt-2 h-10 w-full shadow-md"
+            >
+              {MoodsCategoryPalette({ code: data as any })}
+              <div
+                aria-hidden
+                className="text-content-neutral-inverse absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 cursor-default"
+              >
+                {selectedMood?.NAME}
               </div>
             </div>
           )}
